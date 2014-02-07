7f71fdb67bccd03fa7bb3b36dc77486a53c681f9
uzuzjmd <julian.dehne@gmail.com>
2014-02-07 19:33:15 +0100

    importer erweitert, so dass daten zusammegeführt werden (bis auf Hierarchie)
    Former-commit-id: f3472bd7efd9da6b76aac232a6caa172a5ad41b0

--- competence_data/src/arbeitsagentur/ImportTester.java
+++ competence_data/src/arbeitsagentur/ImportTester.java
@@ -1,5 +1,7 @@
 package arbeitsagentur;
 
+import java.util.List;
+
 import javax.xml.bind.JAXBException;
 
 import org.junit.Test;
@@ -9,28 +11,46 @@ import arbeitsagentur.generated.berufe.Beruf;
 import arbeitsagentur.generated.berufe.Berufe;
 import arbeitsagentur.generated.kompetenzen.Kompetenzen;
 import arbeitsagentur.generated.kompetenzen.Kompmerkmal;
+import arbeitsagentur.generated.kompsuchworte.Suchwort;
+import arbeitsagentur.generated.kompsuchworte.Suchworte;
 
 public class ImportTester {
 
-	//@Test
+	// @Test
 	public void testBerufe() throws JAXBException {
 		XMLImporter importer = new XMLImporter();
 		Berufe berufeImported = importer.importBerufe();
-		for (Beruf beruf: berufeImported.getBeruf()) {
+		for (Beruf beruf : berufeImported.getBeruf()) {
 			System.out.println(beruf.getName());
 		}
 	}
-	
-	@Test
+
+	// @Test
 	public void testKompetenzen() throws JAXBException {
 		XMLImporter importer = new XMLImporter();
 		Kompetenzen berufeImported = importer.importKompetenzen();
-		for (Kompmerkmal beruf: berufeImported.getKompmerkmal()) {
+		for (Kompmerkmal beruf : berufeImported.getKompmerkmal()) {
 			System.out.println(beruf.getBezeichnung());
 		}
 	}
-	
-	
-	
+
+	// @Test
+	public void testSuchworte() throws JAXBException {
+		XMLImporter importer = new XMLImporter();
+		Suchworte suchworte = importer.importSuchworte();
+		for (Suchwort suchwort : suchworte.getSuchwort()) {
+			System.out.println(suchwort.getContent());
+		}
+	}
+
+	 @Test
+	public void testFillKompetenz() throws JAXBException {
+		XMLImporter importer = new XMLImporter();
+		List<FullCompetence> competencies = importer
+				.importfullKomptenzdefinitions();
+		// for (FullCompetence fullCompetence : competencies) {
+		// System.out.println(fullCompetence.toString());
+		// }
+	}
 
 }
