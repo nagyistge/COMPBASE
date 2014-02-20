597d70f9ab2b4af280bb1a29ea104f62d989f96d
uzuzjmd <julian.dehne@gmail.com>
2014-02-20 23:42:02 +0100

    mit jenabean noch ein wenig herumgespielt

--- owl/src/OntologyManagerTest.java
+++ owl/src/OntologyManagerTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.junit.Test;
 
 import uzuzjmd.owl.util.OntologyManager;
@@ -8,7 +11,7 @@ import uzuzjmd.owl.util.OntologyManager;
 public class OntologyManagerTest {
 
 	@Test
-	public void test() {
+	public void test() throws IOException {
 		OntologyManager ontologyManager = new OntologyManager();
 		ontologyManager.loadOntology();
 	}
