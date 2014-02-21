74a70520e614fa488ac8e0bf359d23885e0fc9cd
uzuzjmd <julian.dehne@gmail.com>
2014-02-21 18:00:44 +0100

    fixed null-individuals issue

--- owl/src/uzuzjmd/owl/persistence/OWLPersistence.java
+++ owl/src/uzuzjmd/owl/persistence/OWLPersistence.java
@@ -1,8 +1,12 @@
 package uzuzjmd.owl.persistence;
 
 import org.semanticweb.owlapi.model.OWLOntology;
+import com.hp.hpl.jena.ontology.OntModel;
 
 public interface OWLPersistence {
 	public void persistOntology(OWLOntology ontology);
 	public OWLOntology loadOntology();
+	public void persistOntologyJena(OntModel ontology);
+	public OntModel loadOntologyJena();
+	
 }
