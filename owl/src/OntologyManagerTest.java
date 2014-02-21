b240e10dc344cf6da181b5435a8604d0862b7d59
uzuzjmd <julian.dehne@gmail.com>
2014-02-21 18:00:44 +0100

    fixed null-individuals issue
    Former-commit-id: 74a70520e614fa488ac8e0bf359d23885e0fc9cd

--- owl/src/OntologyManagerTest.java
+++ owl/src/OntologyManagerTest.java
@@ -1,19 +1,42 @@
-import static org.junit.Assert.*;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.junit.Test;
 
 import uzuzjmd.owl.util.OntologyManager;
+import uzuzjmd.owl.util.OntologyUtil;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
 
 
 public class OntologyManagerTest {
 
+	
 	@Test
-	public void test() throws IOException {
-		OntologyManager ontologyManager = new OntologyManager();
-		ontologyManager.loadOntology();
+	public void testBaseOntology() {
+		OntologyManager manager = new OntologyManager();
+		manager.createBaseOntology();
 	}
+	
+	//@Test
+	public void test() throws IOException {
+
+		OntologyUtil ontologyUtil = new OntologyUtil();
+		
+		OntModel m = ontologyUtil.initializeOntologyModel();
+		ontologyUtil.readFileOntology(m);
+
+		// create a dummy paper for this example
+		String className = "Competence";
+		OntClass paper = ontologyUtil.getOntClassForString(m, className);
+		
+		String individualName = "competence1";
+		Individual p1 = ontologyUtil.createIndividualForString(m, paper, individualName);
+	
+
+	}
+
+	
 
 }
