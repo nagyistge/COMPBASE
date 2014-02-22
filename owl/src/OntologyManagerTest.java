4729f8d04aaaf6d2da6e58fadb66db6184df4bdb
uzuzjmd <julian.dehne@gmail.com>
2014-02-22 02:31:29 +0100

    Classen und Object-Properties sind nun leicht zu erstellen
    Former-commit-id: 0c0405b8f4193fb1bc67f052568315c9773331e1

--- owl/src/OntologyManagerTest.java
+++ owl/src/OntologyManagerTest.java
@@ -3,40 +3,15 @@ import java.io.IOException;
 import org.junit.Test;
 
 import uzuzjmd.owl.util.CompOntologyManager;
-import uzuzjmd.owl.util.CompOntologyUtil;
-
-import com.hp.hpl.jena.ontology.Individual;
-import com.hp.hpl.jena.ontology.OntClass;
-import com.hp.hpl.jena.ontology.OntModel;
 
 
 public class OntologyManagerTest {
-
 	
 	@Test
-	public void testBaseOntology() throws IOException {
-		CompOntologyUtil ontologyUtil = new CompOntologyUtil();
-		CompOntologyManager manager = new CompOntologyManager();
-		OntModel m = manager.createBaseOntology();
-		ontologyUtil.writeOntologyout(m);
+	public void testBaseOntology() throws IOException {		
+		CompOntologyManager manager = new CompOntologyManager();		
+		manager.createBaseOntology();
+		manager.getUtil().writeOntologyout();
 	}
 	
-	//@Test
-	public void test() throws IOException {
-
-		CompOntologyUtil ontologyUtil = new CompOntologyUtil();
-		
-		OntModel m = ontologyUtil.initializeOntologyModel();
-		ontologyUtil.readFileOntology(m, "proof-of-conceptrdf.owl");
-
-		// create a dummy paper for this example
-		String className = "Competence";
-		OntClass paper = ontologyUtil.getOntClassForString(m, className);
-		
-		String individualName = "competence1";
-		Individual p1 = ontologyUtil.createIndividualForString(m, paper, individualName);	
-	}
-
-	
-
 }
