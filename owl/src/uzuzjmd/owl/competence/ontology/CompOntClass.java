d27b1a5b337aff4a634c91cf31152ed45f9de4a5
uzuzjmd <julian.dehne@gmail.com>
2014-02-22 01:36:54 +0100

    Entscheidung für Jena-Api und entsprechendes Aufräumen
    Former-commit-id: 7bc4d4a3990a67eff96f1ef8b3f4495479f26992

---
+++ owl/src/uzuzjmd/owl/competence/ontology/CompOntClass.java
@@ -0,0 +1,13 @@
+package uzuzjmd.owl.competence.ontology;
+
+public enum CompOntClass {
+	Competence,
+	Evidence,
+	Operator,
+	Catchword,
+	CompetenceDescription,
+	DescriptionElement,
+	CompetenceArea,
+	Learner,
+	CompetenceSpec
+}
