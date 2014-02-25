d529f0c878fbd34c293cc1f7b60a68d8da3a16f2
uzuzjmd <julian.dehne@gmail.com>
2014-02-25 13:02:05 +0100

    Bereinigung von CSV implementiert
    Former-commit-id: 199fde1ce6bfcc5b7f5719ba7f393bdf6a426e3a

--- comptetence-main/src/uzuzjmd/csv/competence/CompetenceImporter.scala
+++ comptetence-main/src/uzuzjmd/csv/competence/CompetenceImporter.scala
@@ -9,22 +9,22 @@ import scala.collection.JavaConverters._
 object CompetenceImporter {
 
   def main(args: Array[String]) {
+    // using csv library to map csv to java
     val strat = new ColumnPositionMappingStrategy[CompetenceBean];
     strat.setType(classOf[CompetenceBean])
     val columns = "operator" :: "catchword" :: "competence" :: "similarCompetence" :: "superCompetence" :: "reference" :: "evidenzen" :: "competenceArea" :: "metaoperator" :: Nil;
     strat.setColumnMapping(columns.toArray);
-
     val csv = new CsvToBean[CompetenceBean];
-
     val list = csv.parse(strat, new FileReader(new File(args.head)));
-    val filteredlist = list.asScala.
-    map(CompetenceMaps.comptenceBeansToFilteredCSVCompetences). // erstellt neue Klasse
-    map(a=>a.catchwordsFiltered.filter(CompetenceFilter.catchwordString)); //filtered leere Catchwords und Überschrift
-      
-      ; //simples Erstellen, des neuen Objektes
-        
-     
-
+    
+    //using scala maps to clean the entries
+    val filteredlist = list.asScala. //java list nach scala list
+    map(CompetenceMaps.comptenceBeansToFilteredCSVCompetences). // erstellt neue Klasse    
+    map(a=>a.copy(catchwordsFiltered=a.catchwordsFiltered.filter(CompetenceFilter.catchwordString))).  //filtered leere Catchwords und Überschrift
+    map(a=>a.copy(operator=CompetenceMaps.cleanOperator(a.operator))). //bereinigt den Operator (es darf nur einen geben)
+    map(a=>a.copy(competence=CompetenceMaps.cleanHTML(a.competence))). //bereinigt HTML-Content aus der Kompetenzbeschreibung;
+    map(a=>a.copy(evidencen=CompetenceMaps.cleanHTML(a.evidencen)))
+    filteredlist.foreach(a=>println("\n\n" + a + "\ncatchwords: \n"+ (""::a.catchwordsFiltered).reduce((b,c)=>b+ ","+c).replaceFirst(",", "")))        
   }
 
 }
--- comptetence-main/src/uzuzjmd/csv/competence/CompetenceMaps.scala
+++ comptetence-main/src/uzuzjmd/csv/competence/CompetenceMaps.scala
@@ -1,15 +1,37 @@
 package uzuzjmd.csv.competence
 
 import scala.collection.mutable.Buffer
+import net.htmlparser.jericho;
+import net.htmlparser.jericho.Segment
+import net.htmlparser.jericho.Source
+import net.htmlparser.jericho.Renderer
+
 
 object CompetenceMaps {
   def comptenceBeansToFilteredCSVCompetences(competenceBeans: CompetenceBean): FilteredCSVCompetence = {
-    return new FilteredCSVCompetence(competenceBeans.getCompetence(), competenceBeans.getCatchword().split(",").toList, competenceBeans.getOperator())
+    return new FilteredCSVCompetence(
+        competenceBeans.getCompetence(),
+        competenceBeans.getCatchword().split(",").toList,
+        competenceBeans.getOperator(),
+        competenceBeans.getMetaoperator(),
+        competenceBeans.getEvidenzen(),
+        competenceBeans.getCompetenceArea().split("##").toList)
   }
 
   def cleanCatchwords(catchword: String): String = {
     return catchword.replace("", " ");
   }
+  
+   def cleanOperator(catchword: String): String = {
+    return catchword.split(",").head.replaceAll(" ", "");
+  }
+   
+   def cleanHTML(htmlText:String) : String = {
+     val htmlSource = new Source(htmlText);
+     val htmlSeg = new Segment(htmlSource, 0, htmlText.length());
+     val htmlRend = new Renderer(htmlSeg);
+     return htmlRend.toString();
+   } 
 
 }	
 
