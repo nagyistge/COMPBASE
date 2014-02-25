bfd53008f0cb5d1f690ba2b510d0e846d5a97d48
uzuzjmd <julian.dehne@gmail.com>
2014-02-25 02:25:05 +0100

    aufgeräumt
    ComptenceFilter von Moodle angefangen
    
    
    Former-commit-id: ee4e03b710025e6c9e3b2a2435d634e26ad4b4bf

---
+++ comptetence-main/src/uzuzjmd/csv/competence/FilteredCSVCompetence.scala
@@ -0,0 +1,7 @@
+package uzuzjmd.csv.competence
+
+import scala.collection.immutable.List
+
+class FilteredCSVCompetence (val competence : String, val catchwordsFiltered : List[String], val operator : String) {
+  
+}
--- ontology-example/src/util/OntologyException.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class OntologyException extends RuntimeException {
-	private static final long serialVersionUID = 8896183520950995890L;
-
-	public OntologyException(String message) {
-		super(message);
-	}
-}
