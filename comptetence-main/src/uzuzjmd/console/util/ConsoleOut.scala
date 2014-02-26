2c3f5b9f93db252bf622a28c31e852da1848c1e2
uzuzjmd <julian.dehne@gmail.com>
2014-02-26 15:50:03 +0100

    Implemented Webservice for rcdeo standard
    Former-commit-id: f6c4e6df36cb2fc734495e0d8b7dc0b05fc3eee9

--- comptetence-main/src/uzuzjmd/console/util/ConsoleOut.scala
+++ comptetence-main/src/uzuzjmd/console/util/ConsoleOut.scala
@@ -6,15 +6,20 @@ import uzuzjmd.rcd.generated.Definition
 import uzuzjmd.rcd.generated.Statement
 import scala.collection.JavaConverters._
 import scala.collection.mutable.Buffer
+import java.util.ArrayList
 
 object ConsoleOut {
-	 def printFilteredComptences(filteredList : Buffer[FilteredCSVCompetence]) {
-    filteredList.foreach(a=>println("\n\n" + a + "\ncatchwords: \n"+ (""::a.catchwordsFiltered).reduce((b,c)=>b+ ","+c).replaceFirst(",", "")))
+  def printFilteredComptences(filteredList: Buffer[FilteredCSVCompetence]) {
+    filteredList.foreach(a => println("\n\n" + a + "\ncatchwords: \n" + ("" :: a.catchwordsFiltered).reduce((b, c) => b + "," + c).replaceFirst(",", "")))
   }
   
+  def printRcdeoCompetences(rcdeoCompetences: ArrayList[Rdceo]) {
+	  printRcdeoCompetences(rcdeoCompetences.asScala)
+  }
+
   def printRcdeoCompetences(rcdeoCompetences: Buffer[Rdceo]) {
     rcdeoCompetences.foreach(
-      x => println("\n\n\n"+"Kompetenz " +
+      x => println("\n\n\n" + "Kompetenz " +
         x.getTitle().getLangstring().asScala.head.getValue() +
         " \nKompetenzBody: \n" +
         x.getDefinition().asScala.map(a => definitionToString(a)).+=("").reduce((a, b) => a + b)))
@@ -26,6 +31,6 @@ object ConsoleOut {
 
   def statementToString(statement: Statement): String = {
     return "Name: " + statement.getStatementname() + ", " +
-      "Text: "+ statement.getStatementtext().getLangstring().asScala.head.getValue() + "\n";      
+      "Text: " + statement.getStatementtext().getLangstring().asScala.head.getValue() + "\n";
   }
 }
