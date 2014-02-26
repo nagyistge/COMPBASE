e89fd6dbd3101efd7eedb9cd2436a2ce3807e4d5
uzuzjmd <julian.dehne@gmail.com>
2014-02-26 19:14:50 +0100

    kleine Änderungen
    Former-commit-id: cd3304613a20a519e1cc448565d9238567423045

--- comptetence-main/src/uzuzjmd/service/competence/WebserviceStarter.java
+++ comptetence-main/src/uzuzjmd/service/competence/WebserviceStarter.java
@@ -5,10 +5,8 @@ import javax.xml.ws.Endpoint;
 import uzuzjmd.owl.util.MagicStrings;
 
 public class WebserviceStarter {
-
 	public static void main(String[] args) {
 		Endpoint.publish(MagicStrings.SERVICEENDPOINT, new CompetenceServiceImpl());
 		System.out.println("publishing wsdl to " + MagicStrings.SERVICEENDPOINT);
 	}
-
 }
