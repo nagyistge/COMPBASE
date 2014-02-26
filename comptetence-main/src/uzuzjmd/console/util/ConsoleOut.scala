124af72d7eae04bc206aa9c670979c2e668b2dcd
uzuzjmd <julian.dehne@gmail.com>
2014-02-26 12:00:05 +0100

    implemented mapping from csv to rcd and created convenienc-classes for output
    Former-commit-id: 86a4121dfdf186fd1ab278e3156d9d054b773422

---
+++ comptetence-main/src/uzuzjmd/console/util/ConsoleOut.scala
@@ -0,0 +1,31 @@
+package uzuzjmd.console.util
+
+import uzuzjmd.csv.competence.FilteredCSVCompetence
+import uzuzjmd.rcd.generated.Rdceo
+import uzuzjmd.rcd.generated.Definition
+import uzuzjmd.rcd.generated.Statement
+import scala.collection.JavaConverters._
+import scala.collection.mutable.Buffer
+
+object ConsoleOut {
+	 def printFilteredComptences(filteredList : Buffer[FilteredCSVCompetence]) {
+    filteredList.foreach(a=>println("\n\n" + a + "\ncatchwords: \n"+ (""::a.catchwordsFiltered).reduce((b,c)=>b+ ","+c).replaceFirst(",", "")))
+  }
+  
+  def printRcdeoCompetences(rcdeoCompetences: Buffer[Rdceo]) {
+    rcdeoCompetences.foreach(
+      x => println("\n\n\n"+"Kompetenz " +
+        x.getTitle().getLangstring().asScala.head.getValue() +
+        " \nKompetenzBody: \n" +
+        x.getDefinition().asScala.map(a => definitionToString(a)).+=("").reduce((a, b) => a + b)))
+  }
+
+  def definitionToString(definition: Definition): String = {
+    return definition.getStatement().asScala.map(a => statementToString(a)).reduce((a, b) => a + b)
+  }
+
+  def statementToString(statement: Statement): String = {
+    return "Name: " + statement.getStatementname() + ", " +
+      "Text: "+ statement.getStatementtext().getLangstring().asScala.head.getValue() + "\n";      
+  }
+}
--- comptetence-main/src/uzuzjmd/csv/competence/CompetenceImporter.scala
+++ comptetence-main/src/uzuzjmd/csv/competence/CompetenceImporter.scala
@@ -5,6 +5,11 @@ import au.com.bytecode.opencsv.bean.CsvToBean
 import java.io.FileReader
 import java.io.File
 import scala.collection.JavaConverters._
+import uzuzjmd.rcd.mapper.CSV2RCD
+import uzuzjmd.rcd.generated.Statement
+import uzuzjmd.rcd.generated.Definition
+import uzuzjmd.rcd.generated.Rdceo
+import uzuzjmd.console.util.ConsoleOut
 
 object CompetenceImporter {
 
@@ -16,15 +21,25 @@ object CompetenceImporter {
     strat.setColumnMapping(columns.toArray);
     val csv = new CsvToBean[CompetenceBean];
     val list = csv.parse(strat, new FileReader(new File(args.head)));
-    
+
     //using scala maps to clean the entries
-    val filteredlist = list.asScala. //java list nach scala list
-    map(CompetenceMaps.comptenceBeansToFilteredCSVCompetences). // erstellt neue Klasse    
-    map(a=>a.copy(catchwordsFiltered=a.catchwordsFiltered.filter(CompetenceFilter.catchwordString))).  //filtered leere Catchwords und Überschrift
-    map(a=>a.copy(operator=CompetenceMaps.cleanOperator(a.operator))). //bereinigt den Operator (es darf nur einen geben)
-    map(a=>a.copy(competence=CompetenceMaps.cleanHTML(a.competence))). //bereinigt HTML-Content aus der Kompetenzbeschreibung;
-    map(a=>a.copy(evidencen=CompetenceMaps.cleanHTML(a.evidencen)))
-    filteredlist.foreach(a=>println("\n\n" + a + "\ncatchwords: \n"+ (""::a.catchwordsFiltered).reduce((b,c)=>b+ ","+c).replaceFirst(",", "")))        
+    val filteredList = list.asScala. //java list nach scala list
+      map(CompetenceMaps.comptenceBeansToFilteredCSVCompetences). // erstellt neue Klasse    
+      map(a => a.copy(catchwordsFiltered = a.catchwordsFiltered.filter(CompetenceFilter.catchwordString))). //filtered leere Catchwords und Überschrift
+      map(a => a.copy(operator = CompetenceMaps.cleanOperator(a.operator))). //bereinigt den Operator (es darf nur einen geben)
+      map(a => a.copy(competence = CompetenceMaps.cleanHTML(a.competence))). //bereinigt HTML-Content aus der Kompetenzbeschreibung;
+      map(a => a.copy(evidencen = CompetenceMaps.cleanHTML(a.evidencen)))
+      
+    //mapping CSVObjects to RCD
+    val rcdeoCompetences = CSV2RCD.mapCompetence(filteredList)
+    
+    //debuggingOutput
+    ConsoleOut.printFilteredComptences(filteredList)
+    ConsoleOut.printRcdeoCompetences(rcdeoCompetences)
+    
+    
   }
+  
+ 
 
 }
