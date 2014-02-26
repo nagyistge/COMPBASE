782803132ac0e791b6bc2f6d46a0c26f080b5b2d
uzuzjmd <julian.dehne@gmail.com>
2014-02-26 15:50:03 +0100

    Implemented Webservice for rcdeo standard

--- comptetence-main/src/uzuzjmd/owl/util/MagicStrings.java
+++ comptetence-main/src/uzuzjmd/owl/util/MagicStrings.java
@@ -5,4 +5,5 @@ public class MagicStrings {
 	public static final String TDBLocation = "tdb2";
 	public static final String ONTOLOGYFORMAT = "RDF/XML";
 	public static final String ONTOLOGYFILE = "mymodelrdf.owl";	
+	public static final String SERVICEENDPOINT = "http://localhost:8081/WS/Competence";
 }
