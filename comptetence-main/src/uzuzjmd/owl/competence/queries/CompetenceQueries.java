8ae7eccb5dd969b12354f59e55e55a80934fd258
uzuzjmd <julian.dehne@gmail.com>
2014-03-10 10:12:25 +0100

    tests überarbeitet und queries filtern Thing und Resource
    Former-commit-id: 7b5892fdf706093b653b11ce4d73dd9879179104

--- comptetence-main/src/uzuzjmd/owl/competence/queries/CompetenceQueries.java
+++ comptetence-main/src/uzuzjmd/owl/competence/queries/CompetenceQueries.java
@@ -26,7 +26,9 @@ public class CompetenceQueries {
 		this.m = m;
 		queryprefix = "PREFIX comp: <" + MagicStrings.PREFIX + ">\n"
 				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n";
+				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" 
+				+ "PREFIX owl:<http://www.w3.org/2002/07/owl#>\n"
+				+ "PREFIX dc:<http://purl.org/dc/elements/1.1/>\n";
 	}
 	
 	/**
@@ -40,12 +42,53 @@ public class CompetenceQueries {
 	public Set<OntClass> getRelatedClassesForOntClass(String domainClass, final CompObjectProperties compObjectProperties) {
 		Set<OntClass> result = new HashSet<OntClass>();
 		
+		QueryExecution qe = getClassesForRange(domainClass,
+				compObjectProperties, result);
+		
+		getClassesForDomain(domainClass, compObjectProperties, result, qe);				
+		
+		return result;
+	}
+
+	private void getClassesForDomain(String domainClass,
+			final CompObjectProperties compObjectProperties,
+			Set<OntClass> result, QueryExecution qe) {
+		// Create a new query for range
+		String queryString2 = queryprefix + 			
+			"SELECT ?class " +
+			"WHERE {" +
+			"comp:"+compObjectProperties.name() + " rdfs:range  comp:"+domainClass + ".\n"+
+			"comp:"+compObjectProperties.name() + " rdfs:domain ?class" + ".\n"+			
+			"FILTER (?class != owl:Thing)" + ".\n"+
+			"FILTER (?class != rdfs:Resource)" + "\n"+
+			"}";
+
+		Query query2 = QueryFactory.create(queryString2); 
+
+		// Execute the query and obtain results
+		QueryExecution qe2 = QueryExecutionFactory.create(query2, m);
+		ResultSet results2 = qe2.execSelect();
+		   for ( ; results2.hasNext() ; )
+		    {
+		      QuerySolution soln = results2.nextSolution() ;
+		      result.add(soln.getResource("?class").as(OntClass.class));
+		    }
+
+		// Important - free up resources used running the query
+		qe.close();
+	}
+
+	private QueryExecution getClassesForRange(String domainClass,
+			final CompObjectProperties compObjectProperties,
+			Set<OntClass> result) {
 		// Create a new query
 		String queryString = queryprefix + 			
 			"SELECT ?class " +
 			"WHERE {" +
-			"comp:"+compObjectProperties.name() + " rdfs:range ?class." + "\n"+
-			"comp:"+compObjectProperties.name() + " rdfs:domain comp:"+domainClass +
+			"comp:"+compObjectProperties.name() + " rdfs:range ?class" + ".\n"+
+			"comp:"+compObjectProperties.name() + " rdfs:domain comp:"+domainClass + ".\n"+
+			"FILTER (?class != owl:Thing)" + ".\n"+
+			"FILTER (?class != rdfs:Resource)" + "\n"+
 			"}";
 
 		Query query = QueryFactory.create(queryString); 
@@ -61,7 +104,7 @@ public class CompetenceQueries {
 
 		// Important - free up resources used running the query
 		qe.close();
-		return result;
+		return qe;
 	}
 
 
