c516ab35abe24b70d03280d60e0f2a57c4380060
uzuzjmd <julian.dehne@gmail.com>
2014-03-11 14:04:34 +0100

    fixed create suboperator rel

--- comptetence-main/test/uzuzjmd/test/reasoner/RulesReasonerTest.java
+++ comptetence-main/test/uzuzjmd/test/reasoner/RulesReasonerTest.java
@@ -13,8 +13,9 @@ import uzuzjmd.owl.competence.ontology.CompObjectProperties;
 import uzuzjmd.owl.competence.ontology.CompOntClass;
 import uzuzjmd.owl.reasoning.jena.RuleFactory;
 import uzuzjmd.owl.reasoning.jena.SimpleRulesReasoner;
+import uzuzjmd.owl.util.CompFileUtil;
 import uzuzjmd.owl.util.CompOntologyManager;
-import uzuzjmd.owl.util.CompOntologyUtil;
+import uzuzjmd.owl.util.CompOntologyAccess;
 
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntClass;
@@ -27,42 +28,46 @@ import com.hp.hpl.jena.vocabulary.ReasonerVocabulary;
 public class RulesReasonerTest {
 
 	private CompOntologyManager manager;
-	private CompOntologyUtil util;
+	private CompOntologyAccess util;
 	private OntClass learnerClass;
-	private OntClass testCompetence;	
+	private OntClass testCompetence;
 
 	@Before
 	public void setupTestSzenario() throws IOException {
 		manager = initOntology();
 		util = manager.getUtil();
-		
-		manager.getRulesReasoner().getReasoner().setParameter(ReasonerVocabulary.PROPtraceOn, true);		
+
+		manager.getRulesReasoner().getReasoner()
+				.setParameter(ReasonerVocabulary.PROPtraceOn, true);
 		initRulesReasoning(manager.getRulesReasoner());
-	
+
 		initTestData(util); // fügt ein paar TestIndividuals hinzu
-		
-		//transitivReasonerInit();		
-		//initOWLReasoner();
-				
+
+		// transitivReasonerInit();
+		// initOWLReasoner();
+
 		manager.switchOffDebugg();
-		//manager.switchOnDebug();
+		// manager.switchOnDebug();
 	}
-	
+
 	@After
 	public void showDataAsXML() throws IOException {
-		util.writeOntologyout(manager.getM());
+		CompFileUtil compFileUtil = new CompFileUtil(manager.getM());
+		compFileUtil.writeOntologyout(manager.getM());
 	}
 
 	private void initOWLReasoner() {
-		Reasoner reasoner = ReasonerRegistry.getOWLReasoner();		
-		InfModel infmodel = ModelFactory.createInfModel(reasoner, manager.getM());
+		Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+		InfModel infmodel = ModelFactory.createInfModel(reasoner,
+				manager.getM());
 		infmodel.validate();
 		manager.getM().add(infmodel);
 	}
 
 	private void transitivReasonerInit() {
-		Reasoner reasoner = ReasonerRegistry.getTransitiveReasoner();		
-		InfModel infmodel = ModelFactory.createInfModel(reasoner, manager.getM());
+		Reasoner reasoner = ReasonerRegistry.getTransitiveReasoner();
+		InfModel infmodel = ModelFactory.createInfModel(reasoner,
+				manager.getM());
 		infmodel.validate();
 		manager.getM().add(infmodel);
 	}
@@ -76,16 +81,20 @@ public class RulesReasonerTest {
 	public void testGetRelatedIndiduals() {
 		Individual testindividual = util.createIndividualForString(
 				learnerClass, "juliancominglate");
-		Individual testindividual2 = util.createIndividualForString(testCompetence, "myTestkompetence");
-		util.createObjectPropertyWithIndividual(testindividual, testindividual2, CompObjectProperties.LearnerOf);				
-		Set<Individual> individuals = util.getQueries().getRelatedIndividuals(CompObjectProperties.LearnerOf,testindividual2.getLocalName());			
-		
-		System.out.println("found related individuals for TestCompetence+ LearnerOf");
-		for (Individual individual : individuals) {			
+		Individual testindividual2 = util.createIndividualForString(
+				testCompetence, "myTestkompetence");
+		util.createObjectPropertyWithIndividual(testindividual,
+				testindividual2, CompObjectProperties.LearnerOf);
+		Set<Individual> individuals = util.getQueries().getRelatedIndividuals(
+				CompObjectProperties.LearnerOf, testindividual2.getLocalName());
+
+		System.out
+				.println("found related individuals for TestCompetence+ LearnerOf");
+		for (Individual individual : individuals) {
 			System.out.println(individual);
 		}
 		assertTrue(individuals.contains(testindividual));
-		
+
 	}
 
 	@Test
@@ -95,21 +104,21 @@ public class RulesReasonerTest {
 				CompObjectProperties.LearnerOfInverse);
 		Set<OntClass> classes2 = util.getQueries()
 				.getRelatedClassesForOntClass(CompOntClass.Learner.name(),
-						CompObjectProperties.LearnerOf);		
-		System.out.println("found related classes for Learner+ LearnerOf/LearnerOfInverse");
+						CompObjectProperties.LearnerOf);
+		System.out
+				.println("found related classes for Learner+ LearnerOf/LearnerOfInverse");
 		for (OntClass ontClass : classes2) {
 			System.out.println(ontClass);
 		}
 		for (OntClass ontClass : classes) {
 			System.out.println(ontClass);
 		}
-		
-		util.writeOntologyout(manager.getM());
+
 		assertTrue(classes.contains(testCompetence.getSuperClass()));
 		assertTrue(classes2.contains(testCompetence.getSuperClass()));
 		assertTrue(classes.contains(testCompetence));
 		assertTrue(classes2.contains(testCompetence));
-		
+
 	}
 
 	private CompOntologyManager initOntology() {
@@ -118,12 +127,11 @@ public class RulesReasonerTest {
 		return manager;
 	}
 
-	private void initTestData(CompOntologyUtil util) {
+	private void initTestData(CompOntologyAccess util) {
 		// Erstelle Klassen
 		learnerClass = util.createOntClass(CompOntClass.Learner);
 		testCompetence = util.createOntClassForString("TestCompetence");
-		OntClass catchwordClass = util.createOntClass(CompOntClass.Catchword);		
-		
+		OntClass catchwordClass = util.createOntClass(CompOntClass.Catchword);
 
 		// Erstelle Klassenhierarchie
 		testCompetence.addSuperClass(util.getClass(CompOntClass.Competence));
@@ -134,7 +142,7 @@ public class RulesReasonerTest {
 		Individual catchwordIndividual = util.createIndividualForString(
 				catchwordClass, "mycachtword");
 		util.createIndividualForString(learnerClass, "juliancominglate");
-			
+
 	}
 
 	private SimpleRulesReasoner initRulesReasoning(
