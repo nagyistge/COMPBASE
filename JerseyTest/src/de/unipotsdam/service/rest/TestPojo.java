71decb3cae0bc8706340ca93e9c7f20244af3cfa
uzuzjmd <julian.dehne@gmail.com>
2014-03-19 15:49:15 +0100

    adding jersey spike project

---
+++ JerseyTest/src/de/unipotsdam/service/rest/TestPojo.java
@@ -0,0 +1,29 @@
+package de.unipotsdam.service.rest;
+
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class TestPojo {
+
+	private String name;
+
+	private Map jsonmap;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Map getJsonmap() {
+		return jsonmap;
+	}
+
+	public void setJsonmap(Map jsonmap) {
+		this.jsonmap = jsonmap;
+	}
+}
