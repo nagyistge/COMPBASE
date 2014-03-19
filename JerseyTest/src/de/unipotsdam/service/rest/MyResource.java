a6f5b3a62ac27c6139252d2be2291c31383b9f3e
uzuzjmd <julian.dehne@gmail.com>
2014-03-19 15:49:15 +0100

    adding jersey spike project
    Former-commit-id: 40d35c52719416ee0cf71eb99e42043f297e512a

---
+++ JerseyTest/src/de/unipotsdam/service/rest/MyResource.java
@@ -0,0 +1,34 @@
+package de.unipotsdam.service.rest;
+
+import javax.jws.WebParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("/myresource")
+public class MyResource {
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to
+	 * the client as "text/plain" media type.
+	 * 
+	 * @return String that will be returned as a text/plain response.
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	// @Path("getit")
+	public TestPojo getIt() {
+		// return "Got it!";
+		return new TestPojo();
+	}
+
+	@POST
+	public void addSomethingToServer(@WebParam String input) {
+		System.out.println(input);
+	}
+}
