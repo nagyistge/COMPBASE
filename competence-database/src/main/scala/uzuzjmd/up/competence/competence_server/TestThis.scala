01743c7fae97ba1cf72b8c38c37aca096617e766
uzuzjmd <julian.dehne@gmail.com>
2014-03-20 16:07:14 +0100

    maven und scala integrationsprojekt
    Former-commit-id: daa45662f3c579448c540904d6d29e5bc3f8f8a0

---
+++ competence-database/src/main/scala/uzuzjmd/up/competence/competence_server/TestThis.scala
@@ -0,0 +1,10 @@
+package uzuzjmd.up.competence.competence_server
+
+object TestThis {
+  def printsome() {
+    val start = new Starter
+    start.printsomeJava();
+
+    print("hello scala world")
+  }
+}
