7554372aa65701b3869ce92079e392dfa2f9f4b8
uzuzjmd <julian.dehne@gmail.com>
2014-04-01 19:32:06 +0200

    projekt auf tdb umgestellt

---
+++ competence-database/src/main/scala/uzuzjmd/up/competence/competence_server/FusekiSpike.java
@@ -0,0 +1,22 @@
+package uzuzjmd.up.competence.competence_server;
+
+import java.io.IOException;
+
+import uzuzjmd.owl.util.CompOntologyManager;
+
+public class FusekiSpike {
+
+	public static void main(String[] args) throws IOException {
+		CompOntologyManager compOntologyManager = new CompOntologyManager();
+
+		// compOntologyManager.close();
+		System.out.println("Press enter to exit");
+		System.in.read();
+		// ServerConfig serverConfig = new ServerConfig();
+		// serverConfig.port = 3030;
+		// serverConfig.pages = MagicStrings.TDBLocation;
+		// SPARQLServer sparql = new SPARQLServer(serverConfig);
+		// sparql.start();
+
+	}
+}
