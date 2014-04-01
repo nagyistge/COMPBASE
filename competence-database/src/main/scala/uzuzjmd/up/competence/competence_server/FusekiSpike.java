d9f606c51ef579f8fe8a59340ef3c8bbf37a4b9c
uzuzjmd <julian.dehne@gmail.com>
2014-04-02 00:06:57 +0200

    inmemory fuseki server

--- competence-database/src/main/scala/uzuzjmd/up/competence/competence_server/FusekiSpike.java
+++ competence-database/src/main/scala/uzuzjmd/up/competence/competence_server/FusekiSpike.java
@@ -2,21 +2,33 @@ package uzuzjmd.up.competence.competence_server;
 
 import java.io.IOException;
 
+import org.apache.jena.fuseki.EmbeddedFusekiServer;
+import org.apache.jena.fuseki.server.ServerConfig;
+
 import uzuzjmd.owl.util.CompOntologyManager;
 
 public class FusekiSpike {
 
 	public static void main(String[] args) throws IOException {
+
 		CompOntologyManager compOntologyManager = new CompOntologyManager();
+		// compOntologyManager.unregisterReasoner();
+		compOntologyManager.begin();
 
-		// compOntologyManager.close();
-		System.out.println("Press enter to exit");
-		System.in.read();
-		// ServerConfig serverConfig = new ServerConfig();
-		// serverConfig.port = 3030;
-		// serverConfig.pages = MagicStrings.TDBLocation;
-		// SPARQLServer sparql = new SPARQLServer(serverConfig);
-		// sparql.start();
+		// DatasetGraph dsg = DatasetGraphFactory.createMem();
+		// EmbeddedFusekiServer server = EmbeddedFusekiServer.create(3030,
+		// compOntologyManager.getDataset().asDatasetGraph(),
+		// MagicStrings.TDBLocation);
 
+		ServerConfig serverconfig = new ServerConfig();
+		serverconfig.pages = "I:/workspace/jena-fuseki/pages";
+
+		EmbeddedFusekiServer server = EmbeddedFusekiServer.create(3030,
+				compOntologyManager.getDataset().asDatasetGraph(), "comp");
+
+		server.start();
+
+		// // System.out.println("Press enter to exit");
+		// // System.in.read();
 	}
 }
