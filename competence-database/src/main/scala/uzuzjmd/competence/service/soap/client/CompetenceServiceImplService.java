da170eb7b0f07ef8553c82cb13a7720095b1f790
uzuzjmd <julian.dehne@gmail.com>
2014-04-02 11:21:32 +0200

    packages renamed and service stubs implemented (with csv access as tmp)
    Former-commit-id: 583e8dac5f6a4c23b18b4bb36536ebb0b187339e

---
+++ competence-database/src/main/scala/uzuzjmd/competence/service/soap/client/CompetenceServiceImplService.java
@@ -0,0 +1,16 @@
+/**
+ * CompetenceServiceImplService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package uzuzjmd.competence.service.soap.client;
+
+public interface CompetenceServiceImplService extends javax.xml.rpc.Service {
+    public java.lang.String getCompetenceServiceImplPortAddress();
+
+    public uzuzjmd.competence.service.soap.client.CompetenceService getCompetenceServiceImplPort() throws javax.xml.rpc.ServiceException;
+
+    public uzuzjmd.competence.service.soap.client.CompetenceService getCompetenceServiceImplPort(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
