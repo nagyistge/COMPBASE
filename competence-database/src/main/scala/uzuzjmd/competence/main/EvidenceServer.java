ffe6752de62d465b298e84d74411490e8bdceb98
uzuzjmd <julian.dehne@gmail.com>
2014-04-11 10:41:07 +0200

    adding EvidenceServer
    Former-commit-id: cb3a5a0cc2c724e34143ca8ac9e7b7b8007bb854

---
+++ competence-database/src/main/scala/uzuzjmd/competence/main/EvidenceServer.java
@@ -0,0 +1,32 @@
+package uzuzjmd.competence.main;
+
+import javax.xml.ws.Endpoint;
+
+import uzuzjmd.competence.evidence.service.EvidenceServiceImpl;
+import uzuzjmd.competence.owl.access.MagicStrings;
+
+public class EvidenceServer {
+
+	public static void main(String[] args) {
+		if (args.length != 4) {
+			System.out
+					.println("Die Verwendung lautet java -jar EvidenceServerJar moodleurl moodledb adminname adminpassword");
+		} else {
+			String moodleurl = args[0];
+			String moodledb = args[1];
+			String adminname = args[2];
+			String adminpassword = args[3];
+			startServer(moodleurl, moodledb, adminname, adminpassword);
+		}
+
+	}
+
+	private static void startServer(String moodleurl, String moodledb,
+			String adminname, String adminpassword) {
+		Endpoint.publish(MagicStrings.SERVICEENDPOINT, new EvidenceServiceImpl(
+				moodleurl, moodledb, adminname, adminpassword));
+		System.out.println("publishing wsdl to "
+				+ MagicStrings.EVIDENCESERVICEENDPOINT);
+	}
+
+}
--- competence-database/src/main/scala/uzuzjmd/competence/owl/access/MagicStrings.java
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/access/MagicStrings.java
@@ -9,7 +9,8 @@ public class MagicStrings {
 	public static final String ONTOLOGYFORMAT = "RDF/XML";
 	public static final String ONTOLOGYFILE = "mymodelrdf.owl";
 	public static final String SERVICEENDPOINT = "http://localhost:8081/WS/Competence";
-	public static final String RULESFILE = "I:/workspace/Wissensmodellierung/comptetence-main/src/resources/competence.rules";
+	public static final String EVIDENCESERVICEENDPOINT = "http://localhost:8082/WS/Competence/Evidence";
+	public static final String RULESFILE = "C:/dev/scalaworkspace/Wissensmodellierung/comptetence-main/src/resources/competence.rules";
 	public static final String SINGLETONPREFIX = "I";
-	public static final String CSVLOCATION = "I:/workspace/Wissensmodellierung/competence-database/src/main/scala/resources/kompetenzen_moodle.csv";
+	public static final String CSVLOCATION = "C:/dev/scalaworkspace/Wissensmodellierung/competence-database/src/main/scala/resources/kompetenzen_moodle.csv";
 }
