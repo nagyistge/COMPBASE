cb3a5a0cc2c724e34143ca8ac9e7b7b8007bb854
uzuzjmd <julian.dehne@gmail.com>
2014-04-11 10:41:07 +0200

    adding EvidenceServer

---
+++ competence-database/src/main/scala/uzuzjmd/competence/evidence/model/MoodleEvidence.java
@@ -0,0 +1,22 @@
+package uzuzjmd.competence.evidence.model;
+
+public class MoodleEvidence {
+	private String course;
+	private String activityTyp;
+
+	public String getActivityTyp() {
+		return activityTyp;
+	}
+
+	public void setActivityTyp(String activityTyp) {
+		this.activityTyp = activityTyp;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+}
