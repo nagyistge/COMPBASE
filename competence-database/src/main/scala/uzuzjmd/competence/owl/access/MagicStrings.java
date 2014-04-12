941752e21385753b71de90573d72f7a70a85a835
uzuzjmd <julian.dehne@gmail.com>
2014-04-12 13:42:21 +0200

    MoodleEvidenceServer umgebaut, dass er auch REST spricht

--- competence-database/src/main/scala/uzuzjmd/competence/owl/access/MagicStrings.java
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/access/MagicStrings.java
@@ -14,4 +14,5 @@ public class MagicStrings {
 	public static final String SINGLETONPREFIX = "I";
 	public static final String CSVLOCATION = "C:/dev/scalaworkspace/Wissensmodellierung/competence-database/src/main/scala/resources/kompetenzen_moodle.csv";
 	public static final String MOODLEURL = "http://localhost/moodle/";
+	public static final String RESTURL = "http://localhost:8083";
 }
