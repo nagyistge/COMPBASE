7b43198eddff5e33d5631bcdc6624f581e10fee2
uzuzjmd <julian.dehne@gmail.com>
2014-04-15 13:41:24 +0200

    Started implementing treeview generator for userfirst

--- competence-database/src/main/scala/uzuzjmd/competence/evidence/model/Evidence.java
+++ competence-database/src/main/scala/uzuzjmd/competence/evidence/model/Evidence.java
@@ -9,16 +9,19 @@ public class Evidence {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Evidence(String shortname, String string, String userId) {
+	public Evidence(String shortname, String string, String userId,
+			String changed) {
 		this.shortname = shortname;
 		this.url = string;
 		this.userId = userId;
+		this.changed = changed;
 	}
 
 	private String shortname;
 	private String url;
 	private String userId;
 	private String username;
+	private String changed;
 
 	public String getShortname() {
 		return shortname;
@@ -50,5 +53,13 @@ public class Evidence {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getChanged() {
+		return changed;
+	}
+
+	public void setChanged(String changed) {
+		this.changed = changed;
 	}
 }
---
+++ competence-database/src/main/scala/uzuzjmd/competence/evidence/service/rest/dto/AbstractTreeEntry.java
@@ -0,0 +1,46 @@
+package uzuzjmd.competence.evidence.service.rest.dto;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+public abstract class AbstractTreeEntry {
+	private String name;
+	private String qtip;
+	private String icon;
+
+	public AbstractTreeEntry() {
+
+	}
+
+	public AbstractTreeEntry(String name, String qtip, String icon) {
+		this.name = name;
+		this.qtip = qtip;
+		this.icon = icon;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@XmlAttribute
+	public String getName() {
+		return name;
+	}
+
+	@XmlAttribute
+	public String getQtip() {
+		return qtip;
+	}
+
+	public void setQtip(String qtip) {
+		this.qtip = qtip;
+	}
+
+	@XmlAttribute
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+}
