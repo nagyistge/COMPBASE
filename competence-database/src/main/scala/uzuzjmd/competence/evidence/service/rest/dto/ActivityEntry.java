69600b0874bb8150feaa16a35846d89245eb4744
uzuzjmd <julian.dehne@gmail.com>
2014-04-15 22:27:44 +0200

    aktivität-merger gebaut
    Former-commit-id: 086c696b2699270c5ebc7cb749318839ef6c23fd

--- competence-database/src/main/scala/uzuzjmd/competence/evidence/service/rest/dto/ActivityEntry.java
+++ competence-database/src/main/scala/uzuzjmd/competence/evidence/service/rest/dto/ActivityEntry.java
@@ -5,11 +5,22 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 @XmlSeeAlso(AbstractTreeEntry.class)
 public class ActivityEntry extends AbstractTreeEntry {
 
+	private String url;
+
 	public ActivityEntry() {
 	}
 
-	public ActivityEntry(String name, String qtip, String icon) {
+	public ActivityEntry(String name, String qtip, String icon, String url) {
 		super(name, qtip, icon);
+		this.url = url;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
 	}
 
 }
