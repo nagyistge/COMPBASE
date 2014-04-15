1d90b9f2a67fa0aba93a7d2a1678a025068ffb35
uzuzjmd <julian.dehne@gmail.com>
2014-04-15 19:55:19 +0200

    UserTreeMapper angefangen
    Former-commit-id: 1ac8958e6752fe9f4caf0842051db54711675125

--- competence-database/src/main/scala/uzuzjmd/competence/evidence/service/rest/mapper/Evidence2Tree.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/evidence/service/rest/mapper/Evidence2Tree.scala
@@ -8,15 +8,27 @@ import uzuzjmd.competence.evidence.service.rest.dto.ActivityEntry
 import scala.collection.mutable.Buffer
 import scala.collection.JavaConverters._
 import uzuzjmd.competence.evidence.service.moodle.Module
+import uzuzjmd.competence.evidence.service.rest.dto.ActivityTyp
+import uzuzjmd.competence.owl.access.MagicStrings
 
-class Evidence2Tree(moodleResponses: MoodleContentResponseList, moodleEvidences: Array[MoodleEvidence]) {
-  def getUserTrees(): Buffer[UserTree] = {
-    moodleResponses.asScala.map(x => x.getModules().asScala).flatten.map(moodleResponseToUserTree(moodleEvidences.toBuffer));
+case class Evidence2Tree(moodleResponses: MoodleContentResponseList, moodleEvidences: Array[MoodleEvidence]) {
+  def getUserTrees(): java.util.List[UserTree] = {
+    moodleResponses.asScala.map(x => x.getModules().asScala).flatten.
+      filter(x => filterNoMachingEvidenceSets(moodleEvidences.toBuffer)).
+      map(moodleResponseToUserTree(moodleEvidences.toBuffer)).toList.asJava
   }
 
-  def moodleResponseToUserTree(moodleEvidences: Buffer[MoodleEvidence])(moodleResponse: Module): UserTree = {
-    val filteredMoodleEvidences = moodleEvidences.find(x => x.getUrl().equals(moodleResponse.getUrl()))
+  def moodleResponseToUserTree(moodleEvidences: Seq[MoodleEvidence])(moodleResponse: Module): UserTree = {
+    val moodleEvidencesView = moodleEvidences.view;
+    val matchingEvidence = moodleEvidencesView.find(x => x.getUrl().equals(moodleResponse.getUrl()))
+
     // TODO
-    new UserTree()
+    val activities = moodleEvidencesView.map(x => new ActivityEntry(moodleResponse.getName(), "Activität", MagicStrings.ICONPATHGWT + "/appbar.monitor.to.svg"))
+    val activityTypes = moodleEvidencesView.map(x => new ActivityTyp(moodleResponse.getModname(), "Aktivitätstyp", moodleResponse.getModicon(), activities.asJava))
+    new UserTree(matchingEvidence.head.getUsername(), "Benutzer", "http://icons.iconarchive.com/icons/artua/dragon-soft/16/User-icon.png", activityTypes.asJava)
+  }
+
+  def filterNoMachingEvidenceSets(moodleResponse: Module)(x: Buffer[MoodleEvidence]): Boolean = {
+    return x.map(z =>  z.getUrl()).contains(y => y.getUrl().equals(moodleResponse.getUrl()))
   }
 }
