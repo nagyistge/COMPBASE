295bcbed50f0460ba9c3c92c86544d05ab4c0d9f
uzuzjmd <julian.dehne@gmail.com>
2014-04-16 04:58:53 +0200

    refaktorisiert, so dass Soap Server die gleiche Schnittstelle anbietet

--- competence-database/src/main/scala/uzuzjmd/competence/evidence/service/EvidenceService.java
+++ competence-database/src/main/scala/uzuzjmd/competence/evidence/service/EvidenceService.java
@@ -2,9 +2,13 @@ package uzuzjmd.competence.evidence.service;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.ws.rs.PathParam;
 
 import uzuzjmd.competence.evidence.model.Evidence;
 import uzuzjmd.competence.evidence.model.MoodleEvidence;
+import uzuzjmd.competence.evidence.service.moodle.MoodleContentResponse;
+import uzuzjmd.competence.evidence.service.moodle.MoodleContentResponseList;
+import uzuzjmd.competence.evidence.service.rest.dto.UserTree;
 
 @WebService(name = "EvidenceService", targetNamespace = "http://service.evidence.competence.uzuzjmd/")
 public interface EvidenceService {
@@ -16,4 +20,16 @@ public interface EvidenceService {
 
 	@WebMethod
 	MoodleEvidence[] getUserEvidencesforMoodleCourse(String course);
+
+	@WebMethod
+	public abstract UserTree[] getUserTree(@PathParam("course") String course);
+
+	@WebMethod
+	public abstract MoodleEvidence[] getUserEvidencesforMoodleCourseAsTree(@PathParam("course") String course);
+
+	@WebMethod
+	public abstract MoodleContentResponse[] getCourseContentXML(@PathParam("course") String course);
+
+	@WebMethod
+	public abstract MoodleContentResponseList getCourseContent(@PathParam("course") String course);
 }
