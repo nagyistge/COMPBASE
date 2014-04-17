53e4aebb178d56e729c3ace0169ab843cb38a40b
uzuzjmd <julian.dehne@gmail.com>
2014-04-17 18:48:22 +0200

    activity preview funktioniert (ist aber big bowl of mood)

--- competence-gui/competence-webapp/war/js/preview.js
+++
@@ -1,9 +0,0 @@
-function preview(page_url,selector,divwhereto) {
-	$.get(page_url, function(data){
-		   $(divwhereto).html($(data).find(selector).html());
-	})	
-}
-
-function removePreview(divwhereto) {
-	$(divwhereto).remove();
-}
