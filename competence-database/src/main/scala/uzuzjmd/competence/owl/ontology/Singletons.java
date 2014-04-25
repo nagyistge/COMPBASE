5d2f0fe0a8fd4948c2bae34d7d48da58d3a1704c
uzuzjmd <julian.dehne@gmail.com>
2014-04-25 13:31:28 +0200

    aktueller stand
    Conflicts:
    	competence-database/src/main/scala/uzuzjmd/competence/evidence/service/rest/dto/AbstractTreeEntry.java
    	competence-database/src/main/scala/uzuzjmd/competence/owl/access/MagicStrings.java
    
    Former-commit-id: ba1df475bd8de2d744afca1ab977fb99b47ff338

---
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/ontology/Singletons.java
@@ -0,0 +1,7 @@
+package uzuzjmd.competence.owl.ontology;
+
+import uzuzjmd.competence.owl.access.CompOntologyManager;
+
+public class Singletons {
+	public static final CompOntologyManager ontologyManager = new CompOntologyManager();
+}
