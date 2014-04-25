cb9ab7d6a6ae5dad7ec4899d7d6d2b057bf7e741
uzuzjmd <julian.dehne@gmail.com>
2014-04-25 22:18:40 +0200

    implemented xml schnittstelle für competencenliste

--- competence-database/src/main/scala/uzuzjmd/competence/mapper/gui/Ont2CompetenceTree.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/mapper/gui/Ont2CompetenceTree.scala
@@ -6,20 +6,29 @@ import uzuzjmd.competence.owl.access.CompOntologyManager
 import uzuzjmd.competence.owl.ontology.CompOntClass
 import uzuzjmd.competence.owl.access.MagicStrings
 import com.hp.hpl.jena.ontology.OntProperty
+import scala.collection.mutable.Buffer
 
 /**
  * Diese Klasse mappt die Kompetenzen auf einen Baum, der in GWT-anzeigbar ist
  */
 class Ont2CompetenceTree(ontologyManager: CompOntologyManager) {
-  def getComptenceTree(): List[CompetenceTree] = {
+  def getComptenceTree(): java.util.List[CompetenceTree] = {
+    ontologyManager.begin()
+
+    val subCompetences = getSubClassesOfCompetence()
+    val iProperty = ontologyManager.getM.getOntProperty(MagicStrings.PREFIX + "definition")
+    val definitions = subCompetences.map(x => x.getPropertyValue(iProperty))
+    val result = definitions.filterNot(_ == null).map(x => new CompetenceTree(x.asNode().getLiteralValue().toString(), "Kompetenz", "", null));
+    ontologyManager.close()
+
+    // todo implement
+    return result.toList.asJava
+  }
+
+  def getSubClassesOfCompetence(): Buffer[com.hp.hpl.jena.ontology.OntClass] = {
     val competenceClass = ontologyManager.getUtil().getClass(CompOntClass.Competence);
     val competences = competenceClass.listSubClasses().toList().asScala
-    val singleTons = competences.map(x => ontologyManager.getUtil().createSingleTonIndividual(x))
-
-    val iProperty = ontologyManager.getM.getOntProperty(MagicStrings.PREFIX + "definition")
-    val definitions = singleTons.map(x => x.getPropertyValue(iProperty))
-    val result = definitions.filterNot(_ == null).map(x => new CompetenceTree(x.toString(), "Kompetenz", "", null));
-    // todo implement
-    return result.toList
+    return competences
   }
+
 }
---
+++ competence-database/src/test/scala/uzuzjmd/de/rcd/mapper/Ont2CompetenceTreeTest.scala
@@ -0,0 +1,57 @@
+package uzuzjmd.de.rcd.mapper
+
+/*
+Testing the mapping
+*/
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import uzuzjmd.competence.owl.access.CompFileUtil
+import uzuzjmd.competence.main.CompetenceImporter
+import uzuzjmd.competence.owl.access.CompOntologyManager
+import uzuzjmd.competence.owl.access.MagicStrings
+import uzuzjmd.competence.mapper.gui.Ont2CompetenceTree
+
+@RunWith(classOf[JUnitRunner])
+class Ont2CompetenceTreeTest extends FunSuite with ShouldMatchers {
+
+  test("The CompetenceTree should not be empty") {
+    val compOntManag = new CompOntologyManager()
+    compOntManag.switchOnDebug()
+
+    compOntManag.begin()
+    compOntManag.getM().validate()
+    compOntManag.close()
+
+    val mapper = new Ont2CompetenceTree(compOntManag)
+
+    val subClasses = mapper.getSubClassesOfCompetence()
+    subClasses should not be ('empty)
+
+    val competenceTree = mapper.getComptenceTree
+    competenceTree should not be ('empty)
+
+  }
+
+  test("get subClasses should not be empty") {
+    val compOntManag = new CompOntologyManager()
+    val mapper = new Ont2CompetenceTree(compOntManag)
+    compOntManag.begin()
+    val subClasses = mapper.getSubClassesOfCompetence()
+    compOntManag.close()
+    subClasses should not be ('empty)
+  }
+
+  //  test("A non-empty list should not be empty") {
+  //    List(1, 2, 3) should not be ('empty)
+  //    List("fee", "fie", "foe", "fum") should not be ('empty)
+  //  }
+  //
+  //  test("A list's length should equal the number of elements it contains") {
+  //    List() should have length (0)
+  //    List(1, 2) should have length (2)
+  //    List("fee", "fie", "foe", "fum") should have length (4)
+  //  }
+}
+
