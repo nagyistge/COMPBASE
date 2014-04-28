4a5487588eb5d3dbcdd12f73496edcccab7a00b7
uzuzjmd <julian.dehne@gmail.com>
2014-04-28 15:09:14 +0200

    Assoziation von Kompetenzen zu einem Kurskontext
    Former-commit-id: 430574721732a34b6a9bfdee62d1a602af8b64b6

--- competence-database/src/main/scala/uzuzjmd/competence/service/rest/CompetenceServiceRest.java
+++ competence-database/src/main/scala/uzuzjmd/competence/service/rest/CompetenceServiceRest.java
@@ -1,5 +1,6 @@
 package uzuzjmd.competence.service.rest;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,14 +12,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import uzuzjmd.competence.owl.access.CompFileUtil;
+import uzuzjmd.competence.owl.access.CompOntologyAccess;
+import uzuzjmd.competence.owl.access.CompOntologyManager;
+import uzuzjmd.competence.owl.ontology.CompObjectProperties;
+import uzuzjmd.competence.owl.ontology.CompOntClass;
 import uzuzjmd.competence.rcd.generated.Rdceo;
 import uzuzjmd.competence.service.CompetenceServiceImpl;
 import uzuzjmd.competence.service.rest.dto.CatchwordXMLTree;
 import uzuzjmd.competence.service.rest.dto.CompetenceXMLTree;
 import uzuzjmd.competence.service.rest.dto.OperatorXMLTree;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.ObjectProperty;
+import com.hp.hpl.jena.ontology.OntClass;
 
 /**
  * Root resource (exposed at "competences" path)
@@ -116,8 +127,42 @@ public class CompetenceServiceRest {
 	@Consumes(MediaType.APPLICATION_XML)
 	@POST
 	@Path("/coursecontext/xml/crossdomain/{course}")
-	public Response linkCompetencesToCourseContext(@QueryParam(value = "competences") List<String> competences) {
+	public Response linkCompetencesToCourseContext(@PathParam("course") String course, @QueryParam(value = "competences") String competences) {
 		System.out.println("found" + competences);
+		CompOntologyManager compOntologyManager = new CompOntologyManager();
+		compOntologyManager.begin();
+		compOntologyManager.getM().enterCriticalSection(false);
+		CompOntologyAccess util = compOntologyManager.getUtil();
+
+		OntClass courseContextClass = util.createOntClass(CompOntClass.CourseContext);
+		Individual courseContextIndividual = util.createIndividualForString(courseContextClass, course);
+
+		ObjectProperty courseContextOfProperty = compOntologyManager.getM().getObjectProperty(util.encode(CompObjectProperties.CourseContextOf.name()));
+		courseContextIndividual.removeAll(courseContextOfProperty);
+
+		if (competences == null) {
+			throw new WebApplicationException(new Exception("Es wurden keine Kompetenzen übergeben"));
+		}
+		for (String competence : competences.split(",")) {
+			Individual competenceIndividual = util.createSingleTonIndividual(competence);
+			util.createObjectPropertyWithIndividual(courseContextIndividual, competenceIndividual, CompObjectProperties.CourseContextOf);
+		}
+
+		compOntologyManager.getM().leaveCriticalSection();
+
+		compOntologyManager.getM().validate();
+		compOntologyManager.close();
+		compOntologyManager.begin();
+
+		CompFileUtil compFileUtil = new CompFileUtil(compOntologyManager.getM());
+		try {
+			compFileUtil.writeOntologyout();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		compOntologyManager.close();
 
 		// todo stuff here
 		return Response.ok("it has worked").header("Access-Control-Allow-Origin", "*").build();
