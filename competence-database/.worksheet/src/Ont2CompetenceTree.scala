4b8597d5f7243ba392fe9ba0a2b0f80f2e3c7cd5
uzuzjmd <julian.dehne@gmail.com>
2014-04-28 12:43:48 +0200

    filter funktion eingebaut
    Former-commit-id: 7737a679f1e9cd920ccd91bab92459209a43eb20

--- competence-database/.worksheet/src/Ont2CompetenceTree.scala
+++ competence-database/.worksheet/src/Ont2CompetenceTree.scala
@@ -1,39 +1,8 @@
 import uzuzjmd.competence.service.rest.CompetenceServiceRest
 import scala.collection.JavaConverters._
-import uzuzjmd.competence.service.rest.dto.CompetenceTree
-import uzuzjmd.competence.owl.access.CompOntologyManager
-import uzuzjmd.competence.owl.ontology.CompOntClass
-import uzuzjmd.competence.owl.access.MagicStrings
-import com.hp.hpl.jena.ontology.OntProperty
-//import uzuzjmd.competence.owl.access.Singletons
-import uzuzjmd.competence.owl.access.CompFileUtil
-import uzuzjmd.competence.owl.ontology.Helper
 
 
-object Ont2CompetenceTree {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(584); 
-  println("Welcome to the Scala worksheet");$skip(83); 
-  //val ontologyManager = Singletons.ontologyManager
- 
-  val helper = new Helper();System.out.println("""helper  : uzuzjmd.competence.owl.ontology.Helper = """ + $show(helper ));$skip(56); 
-  val ontologyManager = helper.getOntologyManagerInit();System.out.println("""ontologyManager  : uzuzjmd.competence.owl.access.CompOntologyManager = """ + $show(ontologyManager ));$skip(66); 
-  
-  val compFileUtil = new CompFileUtil(ontologyManager.getM());System.out.println("""compFileUtil  : uzuzjmd.competence.owl.access.CompFileUtil = """ + $show(compFileUtil ));$skip(34); 
-  compFileUtil.writeOntologyout();$skip(94); 
-  
-  
-   val competenceClass = ontologyManager.getUtil().getClass(CompOntClass.Competence);System.out.println("""competenceClass  : com.hp.hpl.jena.ontology.OntClass = """ + $show(competenceClass ));$skip(72); ;
-    val competences = competenceClass.listSubClasses().toList().asScala;System.out.println("""competences  : scala.collection.mutable.Buffer[com.hp.hpl.jena.ontology.OntClass] = """ + $show(competences ));$skip(19); val res$0 = 
-		competences.head;System.out.println("""res0: com.hp.hpl.jena.ontology.OntClass = """ + $show(res$0));$skip(21); val res$1 = 
-    competences.size;System.out.println("""res1: Int = """ + $show(res$1));$skip(98); 
-    val singleTons = competences.map(x => ontologyManager.getUtil().createSingleTonIndividual(x));System.out.println("""singleTons  : scala.collection.mutable.Buffer[com.hp.hpl.jena.ontology.Individual] = """ + $show(singleTons ));$skip(240); 
-
-    //val iProperty = ontologyManager.getM.getOntProperty(MagicStrings.PREFIX + "definition")
-                                                   
-    val iProperty = ontologyManager.getM().getProperty(MagicStrings.PREFIX + "definition");System.out.println("""iProperty  : com.hp.hpl.jena.rdf.model.Property = """ + $show(iProperty ));$skip(125); 
-                                                      
-    val definitions = competences.map(x => x.getProperty(iProperty));System.out.println("""definitions  : scala.collection.mutable.Buffer[com.hp.hpl.jena.rdf.model.Statement] = """ + $show(definitions ));$skip(117); 
-    val result = definitions.filterNot(_ == null).map(x => new CompetenceTree(x.toString(), "Kompetenz", "", null));System.out.println("""result  : scala.collection.mutable.Buffer[uzuzjmd.competence.service.rest.dto.CompetenceTree] = """ + $show(result ));$skip(50); ;
-    // todo implement
-    ontologyManager.close()}
-  
+object Ont2CompetenceTree {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(167); 
+ 		val stuff = List.empty[String];System.out.println("""stuff  : List[String] = """ + $show(stuff ));$skip(26); val res$0 = 
+ 		stuff.forall(x=>false);System.out.println("""res0: Boolean = """ + $show(res$0))}
 }
--- competence-database/src/main/scala/uzuzjmd/competence/service/rest/CompetenceServiceWrapper.java
+++ competence-database/src/main/scala/uzuzjmd/competence/service/rest/CompetenceServiceWrapper.java
@@ -1,5 +1,7 @@
 package uzuzjmd.competence.service.rest;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import uzuzjmd.competence.mapper.gui.Ont2CompetenceTree;
@@ -9,33 +11,38 @@ import uzuzjmd.competence.service.rest.dto.CompetenceXMLTree;
 import uzuzjmd.competence.service.rest.dto.OperatorXMLTree;
 
 public class CompetenceServiceWrapper {
-	public static synchronized CompetenceXMLTree[] getCompetenceTree() {
+
+	private static Ont2CompetenceTree initOnt2Mapper(String[] selectedCatchwordArray, String[] selectedOperatorsArray) {
 		CompOntologyManager compOntologyManager = new CompOntologyManager();
-		Ont2CompetenceTree ont2CompetenceTree = new Ont2CompetenceTree(
-				compOntologyManager);
-		List<CompetenceXMLTree> tmpResult = ont2CompetenceTree
-				.getComptenceTree();
+		List<String> catchwordarray = new LinkedList<String>();
+		if (selectedCatchwordArray != null) {
+			catchwordarray = Arrays.asList(selectedCatchwordArray);
+		}
+
+		List<String> operatorArray = new LinkedList<String>();
+		if (selectedOperatorsArray != null) {
+			operatorArray = Arrays.asList(selectedOperatorsArray);
+		}
+		Ont2CompetenceTree ont2CompetenceTree = new Ont2CompetenceTree(compOntologyManager, catchwordarray, operatorArray);
+		return ont2CompetenceTree;
+	}
+
+	public static synchronized OperatorXMLTree[] getOperatorTree(String[] selectedCatchwordArray, String[] selectedOperatorsArray) {
+		Ont2CompetenceTree ont2CompetenceTree = initOnt2Mapper(selectedCatchwordArray, selectedOperatorsArray);
+		OperatorXMLTree[] tmpResult = ont2CompetenceTree.getOperatorXMLTree().toArray(new OperatorXMLTree[0]);
+		return tmpResult;
+	}
+
+	public static synchronized CatchwordXMLTree[] getCatchwordTree(String[] selectedCatchwordArray, String[] selectedOperatorsArray) {
+		Ont2CompetenceTree ont2CompetenceTree = initOnt2Mapper(selectedCatchwordArray, selectedOperatorsArray);
+		CatchwordXMLTree[] tmpResult = ont2CompetenceTree.getCatchwordXMLTree().toArray(new CatchwordXMLTree[0]);
+		return tmpResult;
+	}
+
+	public static CompetenceXMLTree[] getCompetenceTree(String[] selectedCatchwordArray, String[] selectedOperatorsArray) {
+		Ont2CompetenceTree ont2CompetenceTree = initOnt2Mapper(selectedCatchwordArray, selectedOperatorsArray);
+		List<CompetenceXMLTree> tmpResult = ont2CompetenceTree.getComptenceTree();
 		assert (!tmpResult.isEmpty());
 		return tmpResult.toArray(new CompetenceXMLTree[0]);
-	}
-
-	public static synchronized OperatorXMLTree[] getOperatorTree() {
-		CompOntologyManager compOntologyManager = new CompOntologyManager();
-		Ont2CompetenceTree ont2CompetenceTree = new Ont2CompetenceTree(
-				compOntologyManager);
-
-		OperatorXMLTree[] tmpResult = ont2CompetenceTree.getOperatorXMLTree()
-				.toArray(new OperatorXMLTree[0]);
-		return tmpResult;
-	}
-
-	public static synchronized CatchwordXMLTree[] getCatchwordTree() {
-		CompOntologyManager compOntologyManager = new CompOntologyManager();
-		Ont2CompetenceTree ont2CompetenceTree = new Ont2CompetenceTree(
-				compOntologyManager);
-
-		CatchwordXMLTree[] tmpResult = ont2CompetenceTree.getCatchwordXMLTree()
-				.toArray(new CatchwordXMLTree[0]);
-		return tmpResult;
 	}
 }
