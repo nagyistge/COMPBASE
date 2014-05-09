b7f4d40f2d8dd44caf4cbe2a34310ec7d49eb001
uzuzjmd <julian.dehne@gmail.com>
2014-05-09 12:42:30 +0200

    improved daolayer

--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/CompetenceOntologySingletonDao.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/CompetenceOntologySingletonDao.scala
@@ -3,11 +3,16 @@ package uzuzjmd.competence.owl.dao
 import uzuzjmd.competence.owl.ontology.CompOntClass
 import uzuzjmd.competence.owl.access.CompOntologyManager
 import uzuzjmd.competence.owl.access.OntResult
+import com.hp.hpl.jena.ontology.Individual
 
-class CompetenceOntologySingletonDao(comp: CompOntologyManager, compOntClass : CompOntClass) {
+class CompetenceOntologySingletonDao(comp: CompOntologyManager, compOntClass : CompOntClass) extends Dao (comp) {
   val util = comp.getUtil()
 
   def persist(): OntResult = {
     return util.accessSingletonResourceWithClass(compOntClass)
+  }
+  
+  def createIndividual : Individual = {
+    return persist.getIndividual()
   }
 }
