52e30cd28af8e2625364733f4ab2a6ec207f1adb
uzuzjmd <julian.dehne@gmail.com>
2014-05-12 12:32:40 +0200

    implemented datafield api for dao layer

--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/Comment.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/Comment.scala
@@ -4,6 +4,10 @@ import uzuzjmd.competence.owl.ontology.CompOntClass
 import uzuzjmd.competence.owl.access.CompOntologyManager
 
 class Comment(created: Long, text: String, comp: CompOntologyManager) extends CompetenceOntologyDao(comp, CompOntClass.Comment, text.hashCode() + "") {
+
+  def DATECRATED = "datecreated"
+  def TEXT = "text"
+
   @Override
   protected def deleteMore() {
     //TODO
@@ -11,6 +15,7 @@ class Comment(created: Long, text: String, comp: CompOntologyManager) extends Co
 
   @Override
   protected def persistMore() {
-    //TODO  
+    addDataField(DATECRATED, created + "");
+    addDataField(TEXT, text)
   }
 }
