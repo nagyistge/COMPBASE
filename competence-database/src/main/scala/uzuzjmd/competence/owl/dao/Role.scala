3201d24d2b16a56650c74aef8faf25fff638b49e
uzuzjmd <julian.dehne@gmail.com>
2014-05-12 11:43:32 +0200

    dao layer implementiert und getestet
    Former-commit-id: e5959a0820146332a5b8a40177dd152f9dd8daa3

--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/Role.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/Role.scala
@@ -3,6 +3,6 @@ package uzuzjmd.competence.owl.dao
 import uzuzjmd.competence.owl.ontology.CompOntClass
 import uzuzjmd.competence.owl.access.CompOntologyManager
 
-class Role(comp: CompOntologyManager, compOntClass: CompOntClass) extends CompetenceOntologySingletonDao(comp, compOntClass, null){
-  
+abstract class Role(comp: CompOntologyManager, compOntClass: CompOntClass) extends CompetenceOntologySingletonDao(comp, compOntClass) {
+
 }
--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/StudentRole.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/StudentRole.scala
@@ -3,6 +3,12 @@ package uzuzjmd.competence.owl.dao
 import uzuzjmd.competence.owl.ontology.CompOntClass
 import uzuzjmd.competence.owl.access.CompOntologyManager
 
-class StudentRole(comp: CompOntologyManager, var role : Dao) extends Role(comp, CompOntClass.StudentRole) {
-   role =  new Role(comp, CompOntClass.Role)
+class StudentRole(comp: CompOntologyManager) extends Role(comp, CompOntClass.StudentRole) {
+
+  @Override
+  protected def persistMore() {
+    val role = new RoleInstance(comp)
+    role.persist(false)
+    addSuperClass(role)
+  }
 }
