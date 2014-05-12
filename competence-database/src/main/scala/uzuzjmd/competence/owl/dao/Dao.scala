0034aac51f2e321fea672dc088709e23f4a84dd0
uzuzjmd <julian.dehne@gmail.com>
2014-05-12 11:43:32 +0200

    dao layer implementiert und getestet

--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/Dao.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/Dao.scala
@@ -4,7 +4,7 @@ import com.hp.hpl.jena.ontology.Individual
 import uzuzjmd.competence.owl.ontology.CompObjectProperties
 import uzuzjmd.competence.owl.access.CompOntologyManager
 
-abstract class Dao (comp: CompOntologyManager){
+abstract class Dao(comp: CompOntologyManager) {
   def createIndividual: Individual;
 
   def createEdgeWith(edgeType: CompObjectProperties, range: Dao) {
@@ -12,16 +12,30 @@ abstract class Dao (comp: CompOntologyManager){
     val rangeIndividual = range.createIndividual
     comp.getUtil().createObjectPropertyWithIndividual(domainIndividual, rangeIndividual, edgeType)
   }
-  
+
   def deleteEdge(edgeType: CompObjectProperties, range: Dao) {
     val domainIndividual = createIndividual
     val rangeIndividual = range.createIndividual
     comp.getUtil().deleteObjectPropertyWithIndividual(domainIndividual, rangeIndividual, edgeType)
   }
-  
-  def hasEdge(edgeType: CompObjectProperties, range: Dao) : Boolean = {
-    val domainIndividual = createIndividual
-    val rangeIndividual = range.createIndividual
-    comp.getUtil().existsObjectPropertyWithIndividual(domainIndividual, rangeIndividual, edgeType)
+
+  def hasEdge(edgeType: CompObjectProperties, range: Dao): Boolean = {
+    if (this.exists && range.exists) {
+      val domainIndividual = createIndividual
+      val rangeIndividual = range.createIndividual
+      val result = comp.getUtil().existsObjectPropertyWithIndividual(domainIndividual, rangeIndividual, edgeType)
+      return result
+    } else { return false }
   }
+
+  def addSuperClass(superClass: CompetenceOntologySingletonDao) {
+    if (superClass != null) {
+      val ontSuperClass = superClass.persist(true).getOntclass()
+      createIndividual.getOntClass().addSuperClass(ontSuperClass)
+    }
+  }
+
+  protected def persistMore()
+
+  def exists(): Boolean;
 }
