1de5451cb5a4d61b1ac73fbb22fb1c37b493c478
uzuzjmd <julian.dehne@gmail.com>
2014-05-12 13:22:53 +0200

    implemented abstract link creation
    Former-commit-id: 24b217e9a37e14d7c9a9ac3eab15f251a562321b

--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/EvidenceActivity.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/EvidenceActivity.scala
@@ -3,7 +3,11 @@ package uzuzjmd.competence.owl.dao
 import uzuzjmd.competence.owl.ontology.CompOntClass
 import uzuzjmd.competence.owl.access.CompOntologyManager
 
-class EvidenceActivity(val url: String, val printableName: String, comp: CompOntologyManager) extends CompetenceOntologyDao(comp, CompOntClass.EvidenceActivity, url) {
+class EvidenceActivity(val url: String, val printableName: String, comp: CompOntologyManager) extends CompetenceOntologyDao(comp, CompOntClass.EvidenceActivity, printableName) {
+
+  def URL = "url"
+  def PRINTABLENAME = "printableName"
+
   @Override
   protected def deleteMore() {
     //TODO
@@ -11,6 +15,7 @@ class EvidenceActivity(val url: String, val printableName: String, comp: CompOnt
 
   @Override
   protected def persistMore() {
-    //TODO  
+    addDataField(URL, url)
+    addDataField(PRINTABLENAME, printableName)
   }
 }
