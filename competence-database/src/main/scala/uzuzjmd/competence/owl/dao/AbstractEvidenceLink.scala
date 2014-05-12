5fa4a8c39fc191504df8bac382d15e4ac36bab8b
uzuzjmd <julian.dehne@gmail.com>
2014-05-12 13:22:53 +0200

    implemented abstract link creation

--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/AbstractEvidenceLink.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/AbstractEvidenceLink.scala
@@ -3,25 +3,43 @@ package uzuzjmd.competence.owl.dao
 import uzuzjmd.competence.owl.access.CompOntologyManager
 import com.hp.hpl.jena.ontology.Individual
 import uzuzjmd.competence.owl.ontology.CompOntClass
+import uzuzjmd.competence.owl.ontology.CompObjectProperties
 
 class AbstractEvidenceLink(val creator: User,
-  val linkedUsers: User,
+  val linkedUser: User,
   val courseContexts: CourseContext,
   val comments: List[Comment],
   val evidenceActivity: EvidenceActivity,
-  val dateCreated: Long,
-  val isValidated: Boolean,
+  val dateCreated: java.lang.Long,
+  val isValidated: java.lang.Boolean,
   val comp: CompOntologyManager,
   val compOntClass: CompOntClass) extends CompetenceOntologyDao(comp, CompOntClass.AbstractEvidenceLink, creator.name + evidenceActivity.printableName) {
 
+  def CREATED = "datecreated"
+  def ISVALIDATED = "isValidated"
+
   @Override
   protected def deleteMore() {
-    //TODO
+    if (comments != null) {
+      comments.foreach(x => x.delete)
+    }
   }
 
   @Override
   protected def persistMore() {
-
+    if (comments != null) {
+      comments.foreach(x => x.persist)
+      comments.foreach(x => createEdgeWith(x, CompObjectProperties.CommentOf))
+    }
+    linkedUser.persist
+    creator.persist
+    createEdgeWith(CompObjectProperties.createdBy, creator)
+    courseContexts.persist
+    createEdgeWith(CompObjectProperties.LinkOfCourseContext, courseContexts)
+    evidenceActivity.persist
+    createEdgeWith(evidenceActivity, CompObjectProperties.ActivityOf)
+    addDataField(CREATED, dateCreated)
+    addDataField(ISVALIDATED, isValidated)
   }
 
 }
