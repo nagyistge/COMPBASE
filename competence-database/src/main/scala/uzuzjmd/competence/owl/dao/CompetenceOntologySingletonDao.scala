144e24c82e5b349c14308ad11c3c89a7e88ab2ef
uzuzjmd <julian.dehne@gmail.com>
2014-05-12 09:58:57 +0200

    user roles

--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/CompetenceOntologySingletonDao.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/CompetenceOntologySingletonDao.scala
@@ -4,15 +4,21 @@ import uzuzjmd.competence.owl.ontology.CompOntClass
 import uzuzjmd.competence.owl.access.CompOntologyManager
 import uzuzjmd.competence.owl.access.OntResult
 import com.hp.hpl.jena.ontology.Individual
+import com.hp.hpl.jena.ontology.OntClass
 
-class CompetenceOntologySingletonDao(comp: CompOntologyManager, compOntClass : CompOntClass) extends Dao (comp) {
+class CompetenceOntologySingletonDao(comp: CompOntologyManager, compOntClass: CompOntClass, superClass: CompetenceOntologySingletonDao) extends Dao(comp) {
   val util = comp.getUtil()
 
   def persist(): OntResult = {
-    return util.accessSingletonResourceWithClass(compOntClass)
+    val result = util.accessSingletonResourceWithClass(compOntClass)
+    if (superClass != null) {
+      val ontSuperClass = superClass.persist.getOntclass()
+      result.getOntclass().addSuperClass(ontSuperClass)
+    }
+    return result
   }
-  
-  def createIndividual : Individual = {
+
+  def createIndividual: Individual = {
     return persist.getIndividual()
   }
 }
--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/StudentRole.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/StudentRole.scala
@@ -3,6 +3,6 @@ package uzuzjmd.competence.owl.dao
 import uzuzjmd.competence.owl.ontology.CompOntClass
 import uzuzjmd.competence.owl.access.CompOntologyManager
 
-class StudentRole(comp: CompOntologyManager) extends Role(comp, CompOntClass.StudentRole) {
-
+class StudentRole(comp: CompOntologyManager, var role : Dao) extends Role(comp, CompOntClass.StudentRole) {
+   role =  new Role(comp, CompOntClass.Role)
 }
