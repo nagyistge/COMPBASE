01ecf8cedd1e550e37ce2a00946b341725e0b9e5
uzuzjmd <julian.dehne@gmail.com>
2014-05-12 12:32:40 +0200

    implemented datafield api for dao layer

--- competence-database/src/main/scala/uzuzjmd/competence/owl/dao/Dao.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/Dao.scala
@@ -3,6 +3,9 @@ package uzuzjmd.competence.owl.dao
 import com.hp.hpl.jena.ontology.Individual
 import uzuzjmd.competence.owl.ontology.CompObjectProperties
 import uzuzjmd.competence.owl.access.CompOntologyManager
+import uzuzjmd.competence.owl.access.CompOntologyAccess
+import com.hp.hpl.jena.rdf.model.Property
+import com.hp.hpl.jena.rdf.model.Statement
 
 abstract class Dao(comp: CompOntologyManager) {
   def createIndividual: Individual;
@@ -38,4 +41,33 @@ abstract class Dao(comp: CompOntologyManager) {
   protected def persistMore()
 
   def exists(): Boolean;
+
+  def addDataField(key: String, value: Object) {
+    val literal = comp.getM().createProperty(CompOntologyAccess.encode(key));
+    createIndividual.addLiteral(literal, value);
+  }
+  def getDataField(key: String): Object = {
+    val tmpResult = getPropertyPair(key)
+    if (tmpResult._2 == null) {
+      return null;
+    }
+    return tmpResult._2.asTriple().getObject().getLiteralLexicalForm();
+  }
+  def deleteDataField(key: String) = {
+    val tmpResult = getPropertyPair(key)
+    if (tmpResult._2 != null) {
+      comp.getM().remove(tmpResult._2);
+    }
+  }
+
+  def hasDataField(key: String): Boolean = {
+    return getDataField(key) != null
+  }
+
+  private def getPropertyPair(key: String): (Property, Statement) = {
+    val literal = comp.getM().createProperty(CompOntologyAccess.encode(key));
+    val prop: Statement = createIndividual.getProperty(literal);
+    return (literal, prop)
+  }
+
 }
