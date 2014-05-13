fb330969e75f4afabe48e811d2cab9fa7ad1a485
uzuzjmd <julian.dehne@gmail.com>
2014-05-13 11:09:26 +0200

    * fixed abstract evidence link identifier
    * implemented sorting of xml-gui results

---
+++ competence-database/src/main/scala/uzuzjmd/competence/view/xml/TreeEntryComparator.java
@@ -0,0 +1,19 @@
+package uzuzjmd.competence.view.xml;
+
+import java.util.Comparator;
+
+public class TreeEntryComparator implements Comparator<AbstractTreeEntry> {
+
+	@Override
+	public int compare(AbstractTreeEntry arg0, AbstractTreeEntry arg1) {
+		return compare(arg0.getName(), arg1.getName());
+	}
+
+	private int compare(String str1, String str2) {
+		int res = String.CASE_INSENSITIVE_ORDER.compare(str1, str2);
+		if (res == 0) {
+			res = str1.compareTo(str2);
+		}
+		return res;
+	}
+}
