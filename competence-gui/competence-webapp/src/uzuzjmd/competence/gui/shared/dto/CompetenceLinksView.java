9d05645c2752f1dd398fa38a3b0abfa72946944f
uzuzjmd <julian.dehne@gmail.com>
2014-05-20 21:32:38 +0200

    fixed problem with decoding (needed newer restygwt version)
    Former-commit-id: dcfd1a741ce613251ea6b2af334df9a193d4769b

---
+++ competence-gui/competence-webapp/src/uzuzjmd/competence/gui/shared/dto/CompetenceLinksView.java
@@ -0,0 +1,66 @@
+package uzuzjmd.competence.gui.shared.dto;
+
+import java.util.List;
+
+public class CompetenceLinksView {
+	private String abstractLinkId;
+	private String evidenceTitel;
+	private String evidenceUrl;
+	private List<CommentEntry> comments;
+	private Boolean validated;
+
+	public CompetenceLinksView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CompetenceLinksView(String abstractLinkId, String evidenceTitel,
+			String evidenceUrl, List<CommentEntry> comments, Boolean validated) {
+		this.abstractLinkId = abstractLinkId;
+		this.evidenceTitel = evidenceTitel;
+		this.evidenceUrl = evidenceUrl;
+		this.comments = comments;
+		this.comments.addAll(comments);
+		this.validated = validated;
+	}
+
+	public String getEvidenceTitel() {
+		return evidenceTitel;
+	}
+
+	public void setEvidenceTitel(String evidenceTitel) {
+		this.evidenceTitel = evidenceTitel;
+	}
+
+	public String getEvidenceUrl() {
+		return evidenceUrl;
+	}
+
+	public void setEvidenceUrl(String evidenceUrl) {
+		this.evidenceUrl = evidenceUrl;
+	}
+
+	public List<CommentEntry> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentEntry> comments) {
+		this.comments = comments;
+	}
+
+	public Boolean getValidated() {
+		return validated;
+	}
+
+	public void setValidated(Boolean validated) {
+		this.validated = validated;
+	}
+
+	public String getAbstractLinkId() {
+		return abstractLinkId;
+	}
+
+	public void setAbstractLinkId(String abstractLinkId) {
+		this.abstractLinkId = abstractLinkId;
+	}
+
+}
