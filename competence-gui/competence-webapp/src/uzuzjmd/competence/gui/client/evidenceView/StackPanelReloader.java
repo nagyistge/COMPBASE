861abc295e7231ff5b05c860679e8d8c74ce285c
uzuzjmd <julian.dehne@gmail.com>
2014-05-22 20:14:05 +0200

    fixed the show comment error and the typerror in gwt bootstrap alertbase
    Former-commit-id: 28661bb6cf8a060854cf51bf8afb4568213ec567

--- competence-gui/competence-webapp/src/uzuzjmd/competence/gui/client/evidenceView/StackPanelReloader.java
+++ competence-gui/competence-webapp/src/uzuzjmd/competence/gui/client/evidenceView/StackPanelReloader.java
@@ -1,5 +1,7 @@
 package uzuzjmd.competence.gui.client.evidenceView;
 
+import java.util.List;
+
 import org.fusesource.restygwt.client.JsonCallback;
 import org.fusesource.restygwt.client.Method;
 import org.fusesource.restygwt.client.Resource;
@@ -7,6 +9,7 @@ import org.fusesource.restygwt.client.Resource;
 import uzuzjmd.competence.gui.client.ContextFactory;
 import uzuzjmd.competence.gui.client.evidenceView.EvidenceStackPanel.CompetenceLinksMapCodec;
 import uzuzjmd.competence.gui.shared.dto.CompetenceLinksMap;
+import uzuzjmd.competence.gui.shared.dto.CompetenceLinksView;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.json.client.JSONValue;
@@ -17,12 +20,16 @@ class StackPanelReloader {
 	private DecoratedStackPanel decoratedStackPanel;
 	private String username;
 	private ContextFactory context;
+	public Boolean commentEntryWasSuccess;
+	public String commentEntryIdLastUpdated = "";
 
 	public StackPanelReloader(DecoratedStackPanel decoratedStackPanel,
-			String username, ContextFactory context) {
+			String username, ContextFactory context,
+			Boolean commentEntryWasSuccess) {
 		this.decoratedStackPanel = decoratedStackPanel;
 		this.username = username;
 		this.context = context;
+		this.commentEntryWasSuccess = commentEntryWasSuccess;
 	}
 
 	public void reload() {
@@ -30,37 +37,44 @@ class StackPanelReloader {
 		loadEvidencesFromServer(username, context);
 	}
 
+	public void setCommentEntryWasSuccess(Boolean commentEntryWasSuccess) {
+		this.commentEntryWasSuccess = commentEntryWasSuccess;
+	}
+
 	private void loadEvidencesFromServer(final String userName,
 			final ContextFactory context) {
 		Resource resource = new Resource(context.getServerURL()
 				+ "/competences/json/link/overview/" + userName);
-		resource.get().send(new JsonCallback() {
-
-			@Override
-			public void onSuccess(Method arg0, JSONValue arg1) {
-				CompetenceLinksMapCodec codec = GWT
-						.create(CompetenceLinksMapCodec.class);
-				CompetenceLinksMap competenceLinksMap = codec.decode(arg1);
-				for (String competenceName : competenceLinksMap
-						.getMapUserCompetenceLinks().keySet()) {
-					decoratedStackPanel.add(
-							new EvidenceLinkWidget(competenceLinksMap
-									.getMapUserCompetenceLinks().get(
-											competenceName),
-									new StackPanelReloader(decoratedStackPanel,
-											username, context), userName),
-							competenceName);
-				}
-
-			}
-
-			@Override
-			public void onFailure(Method arg0, Throwable arg1) {
-				// TODO Auto-generated method stub
-				Window.alert("wuff:-(1");
-
-			}
-		});
+		resource.get().send(new MyJsonCallBack(this));
 	}
 
+	class MyJsonCallBack implements JsonCallback {
+
+		private StackPanelReloader stackPanelReloader;
+
+		public MyJsonCallBack(StackPanelReloader stackPanelReloader) {
+			this.stackPanelReloader = stackPanelReloader;
+		}
+
+		@Override
+		public void onSuccess(Method arg0, JSONValue arg1) {
+			CompetenceLinksMapCodec codec = GWT
+					.create(CompetenceLinksMapCodec.class);
+			CompetenceLinksMap competenceLinksMap = codec.decode(arg1);
+			for (String competenceName : competenceLinksMap
+					.getMapUserCompetenceLinks().keySet()) {
+				List<CompetenceLinksView> links = competenceLinksMap
+						.getMapUserCompetenceLinks().get(competenceName);
+				decoratedStackPanel.add(new EvidenceLinkWidget(links,
+						stackPanelReloader, username), competenceName);
+			}
+		}
+
+		@Override
+		public void onFailure(Method arg0, Throwable arg1) {
+			// TODO Auto-generated method stub
+			Window.alert("wuff:-(1");
+		}
+
+	}
 }
