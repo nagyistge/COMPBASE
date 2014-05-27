b99786cf3171b86d453458e10521624c6f91269c
uzuzjmd <julian.dehne@gmail.com>
2014-05-27 09:01:51 +0200

    added sorted set to competencelinksmap

--- competence-database/src/main/scala/uzuzjmd/competence/service/rest/dto/CompetenceLinksMap.java
+++ competence-database/src/main/scala/uzuzjmd/competence/service/rest/dto/CompetenceLinksMap.java
@@ -1,6 +1,6 @@
 package uzuzjmd.competence.service.rest.dto;
 
-import java.util.List;
+import java.util.SortedSet;
 import java.util.TreeMap;
 
 import javax.xml.bind.annotation.XmlRootElement;
@@ -10,17 +10,17 @@ public class CompetenceLinksMap {
 	/*
 	 * maps linked competence to the view
 	 */
-	private TreeMap<String, List<CompetenceLinksView>> mapUserCompetenceLinks = new TreeMap<String, List<CompetenceLinksView>>();
+	private TreeMap<String, SortedSet<CompetenceLinksView>> mapUserCompetenceLinks = new TreeMap<String, SortedSet<CompetenceLinksView>>();
 
-	public TreeMap<String, List<CompetenceLinksView>> getMapUserCompetenceLinks() {
+	public TreeMap<String, SortedSet<CompetenceLinksView>> getMapUserCompetenceLinks() {
 		return mapUserCompetenceLinks;
 	}
 
-	public void setMapUserCompetenceLinks(TreeMap<String, List<CompetenceLinksView>> mapUserCompetenceLinks) {
+	public void setMapUserCompetenceLinks(TreeMap<String, SortedSet<CompetenceLinksView>> mapUserCompetenceLinks) {
 		this.mapUserCompetenceLinks = mapUserCompetenceLinks;
 	}
 
-	public CompetenceLinksMap(TreeMap<String, List<CompetenceLinksView>> mapUserCompetenceLinks) {
+	public CompetenceLinksMap(TreeMap<String, SortedSet<CompetenceLinksView>> mapUserCompetenceLinks) {
 		super();
 		this.mapUserCompetenceLinks = mapUserCompetenceLinks;
 	}
