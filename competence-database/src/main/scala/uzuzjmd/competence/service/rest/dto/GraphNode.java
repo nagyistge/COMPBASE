db0ec28fc6eaab935574726e7e720f51dc5d0d20
uzuzjmd <julian.dehne@gmail.com>
2014-05-28 17:52:54 +0200

    added tooltip template
    Former-commit-id: c2cc80593eb21ea1e6bf4f4048d2dbb48008eae3

---
+++ competence-database/src/main/scala/uzuzjmd/competence/service/rest/dto/GraphNode.java
@@ -0,0 +1,42 @@
+package uzuzjmd.competence.service.rest.dto;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class GraphNode implements IsSerializable {
+
+	public GraphNode() {
+	}
+
+	public GraphNode(String label) {
+		this.setLabel(label);
+	}
+
+	private String label;
+
+	@Override
+	public boolean equals(Object obj) {
+		return (((GraphNode) obj).getLabel().equals(getLabel()));
+	}
+
+	@Override
+	public int hashCode() {
+		return Integer.valueOf(getLabel().codePointAt(0));
+	}
+
+	@Override
+	public String toString() {
+		return getLabel();
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = encode(label);
+	}
+
+	private String encode(String label2) {
+		return label2.replaceAll(" ", "-");
+	}
+}
