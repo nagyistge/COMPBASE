32937e45271bd77e14902b725eb098227ab4ef05
uzuzjmd <julian.dehne@gmail.com>
2014-05-28 15:36:19 +0200

    fixed graph style
    Former-commit-id: 586796f8b5dd7c26e74d1fcb31e096189b5c4d15

--- competence-gui/competence-webapp/war/js/competencejs/graphLayouter.js
+++ competence-gui/competence-webapp/war/js/competencejs/graphLayouter.js
@@ -1,22 +1,25 @@
-var redraw, g, renderer;
+var redraw, g, renderer, st;
 function appendGraph(id,width, height, data) {
-////    var width = width - 20;
-////    var height = height - 60;
-//	var width = 500; //todo variabler machen
-//	var height = 400;
 	
     // daten aus json lesen und in Graph einspeisen        
     g = new Graph();
     for( var k=0; k<data.graph.length; k++ ) {
+//        st = {
+//            directed: data.graph[k].directed,        	
+//            label: data.graph[k].label,
+//            "label-style" : {
+//                "font-size": 20
+//            }            
+//        }
         st = {
-            directed: data.graph[k].directed,        	
-            label: data.graph[k].label,
-            "label-style" : {
-                "font-size": 20
-            }            
-        }
-        g.addEdge(data.graph[k].node1, data.graph[k].node2,st);
-       
+                directed: true,        	
+                label: "Voraussetzung für",
+                "label-style" : {
+                    "font-size": 15
+                }            
+            }
+        
+        g.addEdge(data.graph[k].node1, data.graph[k].node2,st);       
     }             
   
 
