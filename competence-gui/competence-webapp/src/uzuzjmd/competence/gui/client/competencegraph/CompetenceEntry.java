4ea2c5a6d9f6d478b37026027bc7983cb0559e04
uzuzjmd <julian.dehne@gmail.com>
2014-06-03 18:01:26 +0200

    solved color propagation in graph tab
    Former-commit-id: 62c8663d511672961358a5d208a4f28907155504

--- competence-gui/competence-webapp/src/uzuzjmd/competence/gui/client/competencegraph/CompetenceEntry.java
+++ competence-gui/competence-webapp/src/uzuzjmd/competence/gui/client/competencegraph/CompetenceEntry.java
@@ -1,11 +1,13 @@
 package uzuzjmd.competence.gui.client.competencegraph;
 
 import com.github.gwtbootstrap.client.ui.Button;
-import com.github.gwtbootstrap.client.ui.Label;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class CompetenceEntry extends Composite {
@@ -13,19 +15,24 @@ public class CompetenceEntry extends Composite {
 	private static CompetenceEntryUiBinder uiBinder = GWT
 			.create(CompetenceEntryUiBinder.class);
 	@UiField
-	Label numberLabel;
-	@UiField
-	Label competenceTextLabel;
-	@UiField
 	Button deleteButton;
 	@UiField
 	Button prerequisiteCreationButton;
+	@UiField
+	HorizontalPanel competenceEntryHorizontalPanel;
+	@UiField
+	SimplePanel competenceNumber;
+	@UiField
+	SimplePanel competenceText;
 
 	interface CompetenceEntryUiBinder extends UiBinder<Widget, CompetenceEntry> {
 	}
 
 	public CompetenceEntry() {
 		initWidget(uiBinder.createAndBindUi(this));
+		this.competenceEntryHorizontalPanel.setStyleName("-1372101603", true);
+		this.competenceNumber.add(new HTML("-1372101603"));
+		this.competenceText.add(new HTML("Ich bin eine Kompetenz"));
 	}
 
 }
