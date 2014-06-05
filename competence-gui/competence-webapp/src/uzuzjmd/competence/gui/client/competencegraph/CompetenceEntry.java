789344279cc80d8dc10004581913967ed2142c67
uzuzjmd <julian.dehne@gmail.com>
2014-06-05 15:45:21 +0200

    implemented prerequisiteCreatebinder (hacking my way)
    Former-commit-id: 92abba1e9f93ebea69c3f5771cdd17d07f017310

--- competence-gui/competence-webapp/src/uzuzjmd/competence/gui/client/competencegraph/CompetenceEntry.java
+++ competence-gui/competence-webapp/src/uzuzjmd/competence/gui/client/competencegraph/CompetenceEntry.java
@@ -1,25 +1,17 @@
 package uzuzjmd.competence.gui.client.competencegraph;
 
-import java.util.LinkedList;
-
-import org.fusesource.restygwt.client.Resource;
-
-import uzuzjmd.competence.gui.client.Competence_webapp;
 import uzuzjmd.competence.gui.client.tabs.GraphTab;
 
 import com.github.gwtbootstrap.client.ui.Button;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.http.client.Request;
-import com.google.gwt.http.client.RequestCallback;
-import com.google.gwt.http.client.RequestException;
-import com.google.gwt.http.client.Response;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -37,8 +29,8 @@ public class CompetenceEntry extends Composite {
 	SimplePanel competenceNumber;
 	@UiField
 	SimplePanel competenceText;
-	private String competenceSelected;
-	private GraphTab graphTab;
+	private PopupPanel deleteCompetencePopup;
+	private PopupPanel createCompetencePopup;
 
 	interface CompetenceEntryUiBinder extends UiBinder<Widget, CompetenceEntry> {
 	}
@@ -48,40 +40,26 @@ public class CompetenceEntry extends Composite {
 		this.competenceEntryHorizontalPanel.setStyleName(id, true);
 		this.competenceNumber.add(new HTML(id));
 		this.competenceText.add(new HTML(text));
-		this.competenceSelected = text;
-		this.graphTab = graphTab;
+		deleteCompetencePopup = new PopupPanel(true, true);
+		deleteCompetencePopup.add(new CompetencePrerequisiteDeleteBinder(
+				deleteCompetencePopup, graphTab, text));
+
+		deleteCompetencePopup.hide();
+
+		createCompetencePopup = new PopupPanel(true, true);
+		createCompetencePopup.add(new CompetencePrerequisiteCreateBinder(
+				createCompetencePopup, graphTab, text));
+
+		createCompetencePopup.hide();
 	}
 
 	@UiHandler("deleteButton")
 	void onDeleteButtonClick(ClickEvent event) {
-		Resource resource = new Resource(
-				Competence_webapp.contextFactory.getServerURL()
-						+ "/competences/json/prerequisite/delete/"
-						+ Competence_webapp.contextFactory.getCourseId());
-		try {
-			resource.addQueryParam("linkedCompetence", competenceSelected)
-					.post().send(new RequestCallback() {
-
-						@Override
-						public void onResponseReceived(Request request,
-								Response response) {
-							graphTab.reload(new LinkedList<String>());
-						}
-
-						@Override
-						public void onError(Request request, Throwable exception) {
-							GWT.log(exception.getMessage());
-						}
-					});
-		} catch (RequestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			GWT.log(e.getMessage());
-		}
-
+		deleteCompetencePopup.show();
 	}
 
 	@UiHandler("prerequisiteCreationButton")
 	void onPrerequisiteCreationButtonClick(ClickEvent event) {
+		createCompetencePopup.show();
 	}
 }
