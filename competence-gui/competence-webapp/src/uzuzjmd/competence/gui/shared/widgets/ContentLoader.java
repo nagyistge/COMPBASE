fbd2fb383f2ddf8028da1fc6f664e62c8c648acf
uzuzjmd <julian.dehne@gmail.com>
2014-06-11 15:35:51 +0200

    implemented maven structure the two projects now depend only on each other
    Former-commit-id: cecb9af6fcc87d7eac530a12b3eb78c19b08bf9b

--- competence-gui/competence-webapp/src/uzuzjmd/competence/gui/shared/widgets/ContentLoader.java
+++
@@ -1,9 +0,0 @@
-package uzuzjmd.competence.gui.shared.widgets;
-
-public interface ContentLoader {
-	public abstract void setId(String id);
-
-	public abstract void setNodeId(String nodeId);
-
-	public abstract void loadContent();
-}
