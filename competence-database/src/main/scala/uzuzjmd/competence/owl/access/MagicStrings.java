a940c745e329945e027fb533c1b07f6f7c564666
uzuzjmd <julian.dehne@gmail.com>
2014-06-12 10:10:43 +0200

    trying to fix the network problem

--- competence-database/src/main/scala/uzuzjmd/competence/owl/access/MagicStrings.java
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/access/MagicStrings.java
@@ -18,8 +18,8 @@ public class MagicStrings {
 	public static final String SINGLETONPREFIX = "I";
 	public static final String CSVLOCATION = "C:/dev/scalaworkspace/Wissensmodellierung/competence-database/src/main/scala/resources/kompetenzen_moodle_utf8.csv";
 	public static final String MOODLEURL = "http://localhost/moodle/";
-	public static final String RESTURL = "http://localhost:8083";
-	public static final String RESTURLCompetence = "http://localhost:8084";
+	public static final String RESTURL = "http://127.0.0.1:8083";
+	public static final String RESTURLCompetence = "http://127.0.0.1:8084";
 	public static final String ICONPATHMOODLE = "icons/WindowsIcons-master/WindowsPhone/svg";
 	public static final String ICONPATHGWT = "icons/WindowsIcons-master/WindowsPhone/svg";
 
