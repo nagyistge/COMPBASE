ed8a3e015966374606b3a02f35b9f8f6501fcbb0
uzuzjmd <julian.dehne@gmail.com>
2014-06-13 14:39:36 +0200

    fixing bug in start script

--- competence-database/src/main/scala/uzuzjmd/competence/owl/access/MagicStrings.java
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/access/MagicStrings.java
@@ -17,7 +17,7 @@ public class MagicStrings {
 	// "C:/dev/scalaworkspace/Wissensmodellierung/comptetence-main/src/resources/competence.rules";
 	public static final String SINGLETONPREFIX = "I";
 	public static final String CSVLOCATION = "C:/dev/scalaworkspace/Wissensmodellierung/competence-database/src/main/scala/resources/kompetenzen_moodle_utf8.csv";
-	public static final String MOODLEURL = "http://localhost/moodle/";
+	public static String MOODLEURL = "http://localhost/moodle/";
 	public static final String RESTURL = "http://localhost:8083";
 	public static final String RESTURLCompetence = "http://localhost:8084";
 	public static final String ICONPATHMOODLE = "icons/WindowsIcons-master/WindowsPhone/svg";
