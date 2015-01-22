8ff49746232138f13f79ca46576ba9a342957cda
uzuzjmd <julian.dehne@gmail.com>
2015-01-22 15:16:56 +0100

    copied the social activity endpoint starter from the test class

--- competence-database/src/main/scala/uzuzjmd/competence/evidence/service/LiferayEvidenceService.java
+++ competence-database/src/main/scala/uzuzjmd/competence/evidence/service/LiferayEvidenceService.java
@@ -1,21 +1,34 @@
 package uzuzjmd.competence.evidence.service;
 
+import java.rmi.RemoteException;
+
+import org.apache.axis.client.Stub;
+
 import uzuzjmd.competence.evidence.model.Evidence;
 import uzuzjmd.competence.evidence.model.MoodleEvidence;
 import uzuzjmd.competence.evidence.service.moodle.MoodleContentResponse;
 import uzuzjmd.competence.evidence.service.moodle.MoodleContentResponseList;
 import uzuzjmd.competence.evidence.service.rest.dto.UserTree;
 
+import com.liferay.portlet.social.model.SocialActivitySoap;
+import com.liferay.portlet.social.service.http.SocialActivityServiceSoapProxy;
+
 public class LiferayEvidenceService implements EvidenceService {
 	private String adminUserName;
 	private String adminPassword;
 	private String liferayURL;
+	private SocialActivityServiceSoapProxy endpoint;
 
 	public LiferayEvidenceService(String adminUserName, String adminPassword, String liferayURL) {
 		super();
 		this.adminUserName = adminUserName;
 		this.adminPassword = adminPassword;
 		this.liferayURL = liferayURL;
+
+		endpoint = new SocialActivityServiceSoapProxy(this.liferayURL + "/api/axis/Portlet_Social_SocialActivityService?wsdl");
+		((Stub) endpoint.getSocialActivityServiceSoap()).setUsername(this.adminUserName);
+		((Stub) endpoint.getSocialActivityServiceSoap()).setPassword(this.adminPassword);
+
 	}
 
 	@Override
@@ -38,7 +51,19 @@ public class LiferayEvidenceService implements EvidenceService {
 
 	@Override
 	public UserTree[] getUserTree(String course) {
-		// TODO Auto-generated method stub
+		SocialActivitySoap[] activities = null;
+		try {
+			activities = endpoint.getGroupUsersActivities(Long.parseLong(course), -1, -1);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// todo convert them into the tree
+
 		return null;
 	}
 
