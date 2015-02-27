af378b30c2731455bc5c044b7debbb66b9968053
uzuzjmd <julian.dehne@gmail.com>
2015-02-27 09:12:06 +0100

    added suggested learning path to ontology

--- competence-database/src/test/scala/uzuzjmd/competence/evidence/service/EvidenceServiceImplTest.java
+++ competence-database/src/test/scala/uzuzjmd/competence/evidence/service/EvidenceServiceImplTest.java
@@ -1,13 +1,11 @@
 package uzuzjmd.competence.evidence.service;
 
-import org.junit.Test;
 
 public class EvidenceServiceImplTest {
 
-	@Test
+	// @Test
 	public void test() {
-		MoodleEvidenceRestServiceImpl evidenceServiceImpl = new MoodleEvidenceRestServiceImpl(
-				"localhost", "bitnami_moodle", "root", "voyager");
+		MoodleEvidenceRestServiceImpl evidenceServiceImpl = new MoodleEvidenceRestServiceImpl("localhost", "bitnami_moodle", "root", "voyager");
 		evidenceServiceImpl.getMoodleEvidences("2", "2");
 	}
 
