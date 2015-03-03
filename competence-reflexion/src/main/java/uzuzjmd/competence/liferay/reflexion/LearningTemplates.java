7173c21c0054165f4ca8c3ccb4c1d47bcbd56327
uzuzjmd <julian.dehne@gmail.com>
2015-03-03 13:23:28 +0100

    added assessment view to reflexion portlet
    Former-commit-id: aa3fbbf27c3523dd5f5b226642d2eabae23db6ad

--- competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/LearningTemplates.java
+++ competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/LearningTemplates.java
@@ -1,15 +1,21 @@
 package uzuzjmd.competence.liferay.reflexion;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name="LearningTemplates")
-@RequestScoped
-public class LearningTemplates {	
+@ViewScoped
+public class LearningTemplates implements Serializable{	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private List<String> learningTemplates;
 
 	public List<String> getLearningTemplates() {
---
+++ competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/ReflectiveAssessmentsListHolder.java
@@ -0,0 +1,69 @@
+package uzuzjmd.competence.liferay.reflexion;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name="ReflectiveAssessmentsListHolder")
+@ViewScoped
+public class ReflectiveAssessmentsListHolder implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@ManagedProperty("#{reflectiveAssessmentList}")
+	private List<ReflectiveAssessment> reflectiveAssessmentList;
+	private String suggestedMetaCompetence;
+	private Assessment assessment;
+
+	public List<ReflectiveAssessment> getReflectiveAssessmentList() {
+		return reflectiveAssessmentList;
+	}
+
+	public void setReflectiveAssessmentList(List<ReflectiveAssessment> reflectiveAssessmentList) {
+		this.reflectiveAssessmentList = reflectiveAssessmentList;
+	}
+
+	public String getSuggestedMetaCompetence() {
+		return suggestedMetaCompetence;
+	}
+
+	public void setSuggestedMetaCompetence(String suggestedMetaCompetence) {
+		this.suggestedMetaCompetence = suggestedMetaCompetence;
+	}
+	
+	@PostConstruct	
+	public void init() {
+		assessment = new Assessment();
+		assessment.init();
+		
+		reflectiveAssessmentList = new LinkedList<ReflectiveAssessment>();
+		
+		ReflectiveAssessment assessment = new ReflectiveAssessment();
+		assessment.setIsLearningGoal(true);
+		HashMap<String, Integer> reflexion = new HashMap<String, Integer>();
+		reflexion.put("Ich kann viele tolle Dinge", 2);
+//		assessment.setReflexion(reflexion);
+		assessment.setCompetenceDescription("some stuff, I don't know");
+		assessment.setAssessmentIndex(0);
+		reflectiveAssessmentList.add(assessment);
+				
+		this.setSuggestedMetaCompetence("Hörbuch hören können");
+		
+	}
+
+	public Assessment getAssessment() {
+		return assessment;
+	}
+
+	public void setAssessment(Assessment assessment) {
+		this.assessment = assessment;
+	}
+}
