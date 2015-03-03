a8ccefc919e485f40fd8670be8f47f96514466c7
uzuzjmd <julian.dehne@gmail.com>
2015-03-03 13:23:28 +0100

    added assessment view to reflexion portlet

--- competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/SuggestedCompetenceColumn.java
+++ competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/SuggestedCompetenceColumn.java
@@ -1,5 +1,7 @@
 package uzuzjmd.competence.liferay.reflexion;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
@@ -7,10 +9,13 @@ import javax.faces.bean.ViewScoped;
 
 @ViewScoped
 @ManagedBean(name="SuggestedCompetenceColumn" , eager = true)
-public class SuggestedCompetenceColumn {
+public class SuggestedCompetenceColumn implements Serializable {
 	
 	@ManagedProperty("#{testOutput}")
 	private String testOutput;
+	
+	@ManagedProperty("#{progressInPercent}")
+	private int progressInPercent;
 	
 	public String getTestOutput() {
 		return testOutput;
@@ -18,6 +23,14 @@ public class SuggestedCompetenceColumn {
 
 	public void setTestOutput(String testOutput) {
 		this.testOutput = testOutput;
+	}
+
+	public int getProgressInPercent() {
+		return progressInPercent;
+	}
+
+	public void setProgressInPercent(int progressInPercent) {
+		this.progressInPercent = progressInPercent;
 	}
 
 }
--- competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/SuggestedCompetenceRow.java
+++ competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/SuggestedCompetenceRow.java
@@ -1,5 +1,6 @@
 package uzuzjmd.competence.liferay.reflexion;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +11,13 @@ import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name="SuggestedCompetenceRow" , eager = true)
 @ViewScoped
-public class SuggestedCompetenceRow {
+public class SuggestedCompetenceRow implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
 	@ManagedProperty("#{suggestedCompetenceColumns}")
 	private List<SuggestedCompetenceColumn> suggestedCompetenceColumns;
 	
