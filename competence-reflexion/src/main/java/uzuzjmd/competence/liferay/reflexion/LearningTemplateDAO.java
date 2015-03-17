8cee0ba0dd0ab2e9a870623242633389f611b040
uzuzjmd <julian.dehne@gmail.com>
2015-03-17 09:51:30 +0100

    Added Concept SelectedLearnignProjectTemplate

--- competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/LearningTemplateDAO.java
+++ competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/LearningTemplateDAO.java
@@ -1,38 +1,31 @@
 package uzuzjmd.competence.liferay.reflexion;
 
-import java.util.Date;
-
-import uzuzjmd.competence.assessment.model.UserLearningTemplateMap;
-import uzuzjmd.competence.assessment.service.UserLearningTemplateMapLocalServiceUtil;
-
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
 
 
 public class LearningTemplateDAO extends LiferayDAO {
 	public static void persist(String selectedLearningTemplate) {
-		UserLearningTemplateMap userLearningTemplateMap = null;
-		try {			
-			try {
-				userLearningTemplateMap = UserLearningTemplateMapLocalServiceUtil.getUserLearningTemplateMap(selectedLearningTemplate.hashCode());
-			} catch (PortalException e1) {
-				userLearningTemplateMap = UserLearningTemplateMapLocalServiceUtil.createUserLearningTemplateMap(selectedLearningTemplate.hashCode());
-			}
-			
-			userLearningTemplateMap.setCompanyId(1l);			
-			userLearningTemplateMap.setCreateDate(new Date(System.currentTimeMillis()));
-			userLearningTemplateMap.setLearningTemplate(selectedLearningTemplate);
-			try {
-				userLearningTemplateMap.setGroupId(getUserLoggedIn().getGroupId());
-			} catch (PortalException e) {				
-				e.printStackTrace();
-			}
-			userLearningTemplateMap.setUserId(getUserLoggedIn().getUserId());
-			userLearningTemplateMap.setModifiedDate(new Date(System.currentTimeMillis()));
-		UserLearningTemplateMapLocalServiceUtil.updateUserLearningTemplateMap(userLearningTemplateMap);
-		} catch (SystemException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
+//		UserLearningTemplateMap userLearningTemplateMap = null;
+//		try {			
+//			try {
+//				userLearningTemplateMap = UserLearningTemplateMapLocalServiceUtil.getUserLearningTemplateMap(selectedLearningTemplate.hashCode());
+//			} catch (PortalException e1) {
+//				userLearningTemplateMap = UserLearningTemplateMapLocalServiceUtil.createUserLearningTemplateMap(selectedLearningTemplate.hashCode());
+//			}
+//			
+//			userLearningTemplateMap.setCompanyId(1l);			
+//			userLearningTemplateMap.setCreateDate(new Date(System.currentTimeMillis()));
+//			userLearningTemplateMap.setLearningTemplate(selectedLearningTemplate);
+//			try {
+//				userLearningTemplateMap.setGroupId(getUserLoggedIn().getGroupId());
+//			} catch (PortalException e) {				
+//				e.printStackTrace();
+//			}
+//			userLearningTemplateMap.setUserId(getUserLoggedIn().getUserId());
+//			userLearningTemplateMap.setModifiedDate(new Date(System.currentTimeMillis()));
+//		UserLearningTemplateMapLocalServiceUtil.updateUserLearningTemplateMap(userLearningTemplateMap);
+//		} catch (SystemException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}		
 	}
 }
