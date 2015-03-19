75be07efd06aec26ef9dd4969c278f11117b3d8e
uzuzjmd <julian.dehne@gmail.com>
2015-03-19 16:08:41 +0100

    added SelfAssessmentDAO
    Former-commit-id: c50bfd4c7f171b8e06f8d586e410a2b2733eb69a

---
+++ competence-database/src/main/scala/uzuzjmd/competence/owl/dao/SelfAssessment.scala
@@ -0,0 +1,40 @@
+package uzuzjmd.competence.owl.dao
+
+import uzuzjmd.competence.owl.access.CompOntologyManager
+import uzuzjmd.competence.owl.ontology.CompOntClass
+import uzuzjmd.competence.owl.access.CompOntologyAccessScala
+import uzuzjmd.competence.owl.ontology.CompObjectProperties
+
+case class SelfAssessment(comp: CompOntologyManager, competence: Competence, user: User, val assmentIndex: java.lang.Integer = null) extends CompetenceOntologyDao(comp, CompOntClass.SelfAssessment, CompOntologyAccessScala.createIdentifierForAssessment(user, competence)) {
+  def ASSESSMENTINDEX = "assessmentIndex"
+
+  @Override
+  def getFullDao(): SelfAssessment = {
+    return new SelfAssessment(comp, competence, user, getDataFieldInt(ASSESSMENTINDEX))
+  }
+
+  @Override
+  def persistMore() {
+    if (assmentIndex != null) {
+      addDataField(ASSESSMENTINDEX, assmentIndex)
+    }
+  }
+
+  @Override
+  def deleteMore() {
+
+  }
+
+  def updateIndex(assmentIndex: Integer) {
+    addDataField(ASSESSMENTINDEX, assmentIndex)
+  }
+
+  def addCompetenceToAssessment(competence: Competence) {
+    createEdgeWith(CompObjectProperties.AssessmentOfCompetence, competence)
+  }
+
+  def addUserToAssessment(user: User) {
+    createEdgeWith(CompObjectProperties.AssessmentOfUser, user)
+  }
+
+}
