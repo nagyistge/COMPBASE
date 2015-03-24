3c4e453d50a4d8a1c66c06f39bf600688ec9410c
uzuzjmd <julian.dehne@gmail.com>
2015-03-24 11:10:02 +0100

    fixed bugs in algorithm
    Former-commit-id: 489107bee7509d34025b19b5ec0fb0f3bc43d7e7

--- competence-database/src/main/scala/uzuzjmd/competence/mapper/gui/filter/Ont2SuggestedCompetencyGridMapper.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/mapper/gui/filter/Ont2SuggestedCompetencyGridMapper.scala
@@ -1,24 +1,43 @@
 package uzuzjmd.competence.mapper.gui.filter
 
-import uzuzjmd.competence.owl.dao.Competence
-import uzuzjmd.competence.owl.dao.Competence
-import scala.collection.mutable.MutableList
-import scala.collection.mutable.LinkedList
 import scala.collection.mutable.Buffer
+import org.apache.log4j.Level
+import org.apache.log4j.LogManager
+import uzuzjmd.competence.console.util.LogStream
+import uzuzjmd.competence.owl.dao.Competence
+import uzuzjmd.competence.mapper.gui.Ont2SuggestedCompetenceGrid
+import uzuzjmd.competence.owl.ontology.CompObjectProperties
 
 object Ont2SuggestedCompetencyGridMapper {
 
   type ComPairList = Buffer[(Competence, Competence)]
 
+  val log = LogManager.getLogger(Ont2SuggestedCompetencyGridMapper.getClass().getName());
+  log.setLevel(Level.WARN)
+  val logStream = new LogStream(log, Level.DEBUG);
+
   def convertListToSuggestedCompetenceTriples(input: Buffer[Competence]): ComPairList = {
     val pairs = input.combinations(2)
     val pairsTMP = pairs.map(x => (x.head, x.last))
-    val pairsTMP2 = pairsTMP.map(x => (x._2, x._1))
-    val pairsTMP3 = (pairsTMP ++ pairsTMP2).toBuffer
-    return pairsTMP3.filter(Ont2SuggestedCompetencyGridFilter.filterisSuggestedCompetency)
+    val filteredResult = pairsTMP.map(reorderPairs).filter(Ont2SuggestedCompetencyGridFilter.filterisSuggestedCompetency).toBuffer
+
+    log.debug("combinations after filtering:")
+    log.debug(Ont2SuggestedCompetenceGrid.compairListToString(filteredResult))
+
+    return filteredResult
+  }
+
+  def reorderPairs(pair: (Competence, Competence)): (Competence, Competence) = {
+    if (pair._2.hasEdge(CompObjectProperties.SuggestedCompetencePrerequisiteOf, pair._1)) {
+      return (pair._2, pair._1)
+    } else {
+      return pair
+    }
   }
 
   def convertListToSuggestedCompetenceTriplesInverse(input: ComPairList): Buffer[Competence] = {
-    return input.map(x => x._1)
+    val result = input.map(x => x._1)
+    result.append(input.last._2)
+    return result
   }
 }
