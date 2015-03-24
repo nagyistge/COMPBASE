489107bee7509d34025b19b5ec0fb0f3bc43d7e7
uzuzjmd <julian.dehne@gmail.com>
2015-03-24 11:10:02 +0100

    fixed bugs in algorithm

--- competence-database/src/main/scala/uzuzjmd/competence/mapper/gui/filter/Ont2SuggestedCompetencyGridFilter.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/mapper/gui/filter/Ont2SuggestedCompetencyGridFilter.scala
@@ -7,4 +7,8 @@ object Ont2SuggestedCompetencyGridFilter {
   def filterisSuggestedCompetency(pair: (Competence, Competence)): Boolean = {
     return pair._1.hasEdge(CompObjectProperties.SuggestedCompetencePrerequisiteOf, pair._2)
   }
+
+  def sortCompetencePairs(a: (Competence, Competence), b: (Competence, Competence)): Boolean = {
+    return filterisSuggestedCompetency((a._2, b._1)) || filterisSuggestedCompetency((b._2, a._1))
+  }
 }
