b1aa7b76f6cb5b43ea680c271999c97f6bf5ed7f
uzuzjmd <julian.dehne@gmail.com>
2015-03-26 14:45:27 +0100

    implemented service
    Former-commit-id: 68f8e52b3429788b81efc94319cda1ad02f8a0ef

--- competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/SuggestedCompetenceGrid.java
+++ competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/SuggestedCompetenceGrid.java
@@ -1,30 +1,31 @@
 package uzuzjmd.competence.liferay.reflexion;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @ManagedBean(name = "SuggestedCompetenceGrid")
 @ViewScoped
-public class SuggestedCompetenceGrid implements Serializable {
+public class SuggestedCompetenceGrid implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
+	
 
 	@ManagedProperty("#{suggestedCompetenceRows}")
 	private List<SuggestedCompetenceRow> suggestedCompetenceRows;
 
-	public SuggestedCompetenceGrid() {	
-	}
+	
+	public SuggestedCompetenceGrid() {
+	}		
 
 	public List<SuggestedCompetenceRow> getSuggestedCompetenceRows() {
 		return suggestedCompetenceRows;
@@ -35,37 +36,4 @@ public class SuggestedCompetenceGrid implements Serializable {
 		this.suggestedCompetenceRows = suggestedCompetenceRows;
 	}
 	
-	
-
-	@PostConstruct
-	public void init() {
-		
-		suggestedCompetenceRows = new ArrayList<SuggestedCompetenceRow>();
-		
-		SuggestedCompetenceRow columntest = new SuggestedCompetenceRow();
-		columntest.setSuggestedCompetenceRowHeader("Hörverstehen");
-		List<SuggestedCompetenceColumn> suggestedCompetenceColumns = new ArrayList<SuggestedCompetenceColumn>();
-		SuggestedCompetenceColumn column = new SuggestedCompetenceColumn();
-		column.setTestOutput("hello Zeile 1.0");
-		column.setProgressInPercent(90);
-		suggestedCompetenceColumns.add(column);
-		columntest.setSuggestedCompetenceColumns(suggestedCompetenceColumns);
-		suggestedCompetenceRows.add(columntest);
-
-		// zweite Testzeile
-
-		SuggestedCompetenceRow columntest2 = new SuggestedCompetenceRow();	
-		columntest2.setSuggestedCompetenceRowHeader("Lesen können");
-		List<SuggestedCompetenceColumn> suggestedCompetenceColumns2 = new ArrayList<SuggestedCompetenceColumn>();
-		SuggestedCompetenceColumn column2 = new SuggestedCompetenceColumn();
-		column2.setTestOutput("hello Zeile 2.1");
-		column2.setProgressInPercent(10);
-		SuggestedCompetenceColumn column3 = new SuggestedCompetenceColumn();
-		column3.setTestOutput("hello Zeile 2.2");
-		column3.setProgressInPercent(40);
-		suggestedCompetenceColumns2.add(column2);
-		suggestedCompetenceColumns2.add(column3);
-		columntest2.setSuggestedCompetenceColumns(suggestedCompetenceColumns2);
-		suggestedCompetenceRows.add(columntest2);
-	}
 }
