71aa4c7cb6209af25beef8441c010b38343400ec
uzuzjmd <julian.dehne@gmail.com>
2015-03-26 14:45:27 +0100

    implemented service

--- competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/LearningTemplates.java
+++ competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/LearningTemplates.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
@@ -17,7 +18,11 @@ public class LearningTemplates implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private List<String> learningTemplates;
-	private String selectedCompetence;
+	private String selectedCompetence; // the one to persist in lernprojekte tab
+	private String selectedLearningTemplate; // the one to select the grid for
+
+	//@ManagedProperty("#{SuggestedCompetenceGrid}")
+	private SuggestedCompetenceGrid suggestedCompetenceGrid;
 
 	public List<String> getLearningTemplates() {
 		return learningTemplates;
@@ -34,18 +39,33 @@ public class LearningTemplates implements Serializable {
 				.getData();
 		if (selectedTemplates != null) {
 			learningTemplates.addAll(selectedTemplates);
+			if (!selectedTemplates.isEmpty()) {
+				SuggestedCompetenceGrid data = SuggestedCompetenceGridDAO.getGrid(selectedTemplates.get(0));
+				System.out.println(data.getSuggestedCompetenceRows().size());
+				setSuggestedCompetenceGrid(data);
+			}
 		}
+
+	}
+
+	public void setSuggestedCompetenceGrid(
+			SuggestedCompetenceGrid suggestedCompetenceGrid) {
+		this.suggestedCompetenceGrid = suggestedCompetenceGrid;
+	}
+
+	public SuggestedCompetenceGrid getSuggestedCompetenceGrid() {
+		return suggestedCompetenceGrid;
 	}
 
 	public void addTemplate(ActionEvent e) {
 		SelectedLearningTemplateDAO.persist(getSelectedCompetence());
 	}
-	
-//	public void deleteTemplate(ActionEvent e) {
-//		System.out.println(e.getComponent().getId());
-//	}
-	
-	public void deleteTemplate1(String todelete) {		
+
+	// public void deleteTemplate(ActionEvent e) {
+	// System.out.println(e.getComponent().getId());
+	// }
+
+	public void deleteTemplate1(String todelete) {
 		SelectedLearningTemplateDAO.delete(todelete);
 	}
 
@@ -55,5 +75,13 @@ public class LearningTemplates implements Serializable {
 
 	public void setSelectedCompetence(String selectedCompetence) {
 		this.selectedCompetence = selectedCompetence;
+	}
+
+	public void setSelectedLearningTemplate(String selectedLearningTemplate) {
+		this.selectedLearningTemplate = selectedLearningTemplate;
+	}
+
+	public String getSelectedLearningTemplate() {
+		return selectedLearningTemplate;
 	}
 }
