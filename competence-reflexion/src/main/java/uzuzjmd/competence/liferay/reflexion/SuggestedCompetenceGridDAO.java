d1001d80a734d51912d68371b38c0d6f5c8d5edd
uzuzjmd <julian.dehne@gmail.com>
2015-03-31 11:41:57 +0200

    seperated rest calls from update problem: solved update problem

--- competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/SuggestedCompetenceGridDAO.java
+++ competence-reflexion/src/main/java/uzuzjmd/competence/liferay/reflexion/SuggestedCompetenceGridDAO.java
@@ -1,49 +1,71 @@
 package uzuzjmd.competence.liferay.reflexion;
 
-import javax.ws.rs.core.MediaType;
+import java.util.Collections;
 
-import uzuzjmd.competence.liferay.util.ContextUtil;
-import uzuzjmd.competence.liferay.util.SOAUtil;
+import com.sun.jersey.spi.inject.SingletonTypeInjectableProvider;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientHandlerException;
-import com.sun.jersey.api.client.UniformInterfaceException;
-import com.sun.jersey.api.client.WebResource;
+//import javax.ws.rs.core.MediaType;
+//
+//import uzuzjmd.competence.liferay.util.ContextUtil;
+//import uzuzjmd.competence.liferay.util.SOAUtil;
+//
+//import com.liferay.portal.kernel.exception.PortalException;
+//import com.liferay.portal.kernel.exception.SystemException;
+//import com.sun.jersey.api.client.Client;
+//import com.sun.jersey.api.client.ClientHandlerException;
+//import com.sun.jersey.api.client.UniformInterfaceException;
+//import com.sun.jersey.api.client.WebResource;
 
 public class SuggestedCompetenceGridDAO {
+
 	public static synchronized SuggestedCompetenceGrid getGrid(
 			String selectedLearningTemplate) {
+		System.out.println("fetching grid for:" + selectedLearningTemplate);
 		
-		System.out.println("fetching grid for: " + selectedLearningTemplate);
-		
-		Client client = com.sun.jersey.api.client.Client.create();
-//		client.setConnectTimeout(1000);
-		WebResource webResource = client.resource(SOAUtil.getRestserverUrl()
-				+ "/competences/xml/learningtemplates/gridview");
-		SuggestedCompetenceGrid result = null;
-		try {
-			result = webResource
-					.queryParam("userId",
-							ContextUtil.getUserLoggedIn().getLogin() + "")
-					.queryParam("groupId",
-							ContextUtil.getGroup().getGroupId() + "")
-					.queryParam("selectedTemplate", selectedLearningTemplate)
-					.accept(MediaType.APPLICATION_XML)
-					.get(SuggestedCompetenceGrid.class);
-		} catch (UniformInterfaceException e) {
-			e.printStackTrace();
-		} catch (ClientHandlerException e) {
-			e.printStackTrace();
-		} catch (PortalException e) {
-			e.printStackTrace();
-		} catch (SystemException e) {
-			e.printStackTrace();
-		}  finally {		
-			client.destroy();
+		if (selectedLearningTemplate.equals("test")) {
+			SuggestedCompetenceGrid result = new SuggestedCompetenceGrid();
+			SuggestedCompetenceRow row = new SuggestedCompetenceRow();
+			SuggestedCompetenceColumn column = new SuggestedCompetenceColumn();
+			column.setProgressInPercent(33);
+			column.setTestOutput("deimudday is here");
+			row.setSuggestedCompetenceColumns(Collections.singletonList(column));
+			result.setSuggestedCompetenceRows(Collections.singletonList(row));
+			return result;
 		}
-		return result;
+		
+		return new SuggestedCompetenceGrid();
 	}
+
+	// public static synchronized SuggestedCompetenceGrid getGrid(
+	// String selectedLearningTemplate) {
+	//
+	// System.out.println("fetching grid for: " + selectedLearningTemplate);
+	//
+	// Client client = com.sun.jersey.api.client.Client.create();
+	// WebResource webResource = client.resource(SOAUtil.getRestserverUrl()
+	// + "/competences/xml/learningtemplates/gridview");
+	// SuggestedCompetenceGrid result = null;
+	// try {
+	// result = webResource
+	// .queryParam("userId",
+	// ContextUtil.getUserLoggedIn().getLogin() + "")
+	// .queryParam("groupId",
+	// ContextUtil.getGroup().getGroupId() + "")
+	// .queryParam("selectedTemplate", selectedLearningTemplate)
+	// .accept(MediaType.APPLICATION_XML)
+	// .get(SuggestedCompetenceGrid.class);
+	// } catch (UniformInterfaceException e) {
+	// e.printStackTrace();
+	// } catch (ClientHandlerException e) {
+	// e.printStackTrace();
+	// } catch (PortalException e) {
+	// e.printStackTrace();
+	// } catch (SystemException e) {
+	// e.printStackTrace();
+	// } finally {
+	// client.destroy();
+	// }
+	// return result;
+	// }
 
 }
