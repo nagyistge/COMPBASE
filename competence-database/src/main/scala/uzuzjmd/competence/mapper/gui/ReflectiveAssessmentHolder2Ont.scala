688981f5a870de3b28003829c9c9fbf8b8309858
uzuzjmd <julian.dehne@gmail.com>
2015-04-07 16:47:16 +0200

    wrote interface for reflexion and update on click
    Former-commit-id: 84605b7d146c708841613bd1d24fc6acb09c4d07

---
+++ competence-database/src/main/scala/uzuzjmd/competence/mapper/gui/ReflectiveAssessmentHolder2Ont.scala
@@ -0,0 +1,12 @@
+package uzuzjmd.competence.mapper.gui
+
+import uzuzjmd.competence.owl.access.CompOntologyManager
+import uzuzjmd.competence.owl.dao.CourseContext
+import uzuzjmd.competence.shared.ReflectiveAssessmentsListHolder
+import uzuzjmd.competence.owl.dao.User
+
+object ReflectiveAssessmentHolder2Ont {
+  def convert(comp: CompOntologyManager, user: User, courseContext: CourseContext, reflectiveAssessmentHolder: ReflectiveAssessmentsListHolder) {
+    // TODO
+  }
+}
