8cb50be59c59f40874e3aed4318832896bc5a8e1
uzuzjmd <julian.dehne@gmail.com>
2015-04-10 11:11:06 +0200

    copied gwt-project to vaadin project

---
+++ competence-portlet/liferay/src/main/webapp/js/competencejs/preview.js
@@ -0,0 +1,76 @@
+$.fn.qtip.styles.preview = {// Last part is the name of the style
+    background: "white",
+    opacity: 1,
+    padding: '30px',
+//    "-moz-opacity":1.0,
+//    "-moz-border-radius": "15px",
+    "padding-top": "60px",
+    margin: "auto",
+    border: {
+        width: 3,
+        radius: 15,
+        color: '#6699CC'
+    },
+    width: {min: 400},
+    height: {min: 400},
+    tip: 'rightMiddle',
+    name: 'cream' // Inherit the rest of the attributes from the preset dark style
+}
+
+/**
+ * Diese Funktion hängt eine PreviewSeite an
+ * 
+ * @param page_url
+ *            die Seite die geladen werden soll
+ * @param selector
+ *            um einen Teil der Seite auszuscheinden
+ * @param divwhereto
+ *            die id des elements, wo die seite angehängt werden soll
+ */
+
+function preview(page_url, selector, divwhereto) {
+    $.get(page_url, function(data) {
+        if ($(data).find(selector).length > 0) {
+        	$(divwhereto).qtip({
+                content: $(data).find(selector).html(),
+                style: {
+                    tip: false, // Give it a speech bubble tip with automatic corner detection                
+                    name: 'preview'
+                }
+            });
+        } else {
+        	$(divwhereto).qtip({
+                content: $(data),
+                style: {
+                    tip: false, // Give it a speech bubble tip with automatic corner detection                
+                    name: 'preview'
+                }
+            });
+        }        
+    });
+}
+
+
+//function preview(page_url,selector,divwhereto) {
+//	$.get(page_url, function(data){
+//		   $(divwhereto).html($(data).find(selector).html());
+//	})	
+//}
+//
+//function removePreview(divwhereto) {
+//	$(divwhereto).remove();
+//}
+
+function previewdebug(page_url, selector, divwhereto) {
+    $(divwhereto).qtip({
+        content: 'Some basic content for the tooltip' + page_url, // Give it some content,        
+        style: {
+            tip: false, // Give it a speech bubble tip with automatic corner detection                
+            name: 'preview'
+        }
+    });
+
+}
+
+// adding some random stuff
+
---
+++ competence-shared/src/main/java/uzuzjmd/competence/service/rest/client/Graph.java
@@ -0,0 +1,109 @@
+package uzuzjmd.competence.service.rest.client;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class Graph {
+	public Set<GraphTriple> triples;
+	public Set<GraphNode> nodes;
+
+	private Map<Integer, String> nodeIdValues = new HashMap<Integer, String>();
+
+	public Map<Integer, String> getNodeIdValues() {
+		return nodeIdValues;
+	}
+
+	public void setNodeIdValues(Map<Integer, String> nodeIdValues) {
+		this.nodeIdValues = nodeIdValues;
+	}
+
+	public Graph() {
+		this.triples = new HashSet<GraphTriple>(10000);
+		this.nodes = new HashSet<GraphNode>(10000);
+	}
+
+	/**
+	 * add new Connection on Graph
+	 * 
+	 * @param fromNode
+	 * @param toNode
+	 * @param label
+	 * @param directed
+	 */
+	public void addTriple(String fromNode, String toNode, String label,
+			Boolean directed) {
+		GraphTriple graphTriple = new GraphTriple(fromNode, toNode, label,
+				directed);
+		nodeIdValues.put(fromNode.hashCode(), fromNode);
+		nodeIdValues.put(toNode.hashCode(), toNode);
+		GraphNode fromNode1 = new GraphNode(fromNode);
+		GraphNode toNode1 = new GraphNode(toNode);
+		this.triples.add(graphTriple);
+		this.nodes.add(fromNode1);
+		this.nodes.add(toNode1);
+	}
+
+	@Override
+	public String toString() {
+		String result = "";
+		for (GraphTriple triple : triples) {
+			result += triple.toString() + "\n";
+		}
+		result += "\n";
+		for (GraphNode graphNode : nodes) {
+			result += graphNode.toString() + "\n";
+		}
+		return result;
+	}
+
+	public void mergeWith(Graph graph) {
+		if (graph == null) {
+			System.err.println("merging null Graph");
+		} else {
+			this.nodes.addAll(graph.nodes);
+			this.triples.addAll(graph.triples);
+		}
+	}
+
+	public Graph addConnectingTriples(Graph graph) {
+		for (GraphTriple graphTriple : graph.triples) {
+			if (this.nodes.contains(new GraphNode(graphTriple.fromNode))) {
+				this.triples.add(graphTriple);
+			}
+		}
+		return this;
+	}
+
+	/**
+	 * the input graph is kept
+	 * 
+	 * @param graph
+	 * @return
+	 */
+	public Graph intersectWith(Graph graph) {
+		this.nodes.retainAll(graph.nodes);
+		this.nodes.addAll(graph.nodes);
+		this.triples.retainAll(graph.triples);
+		this.triples.addAll(graph.triples);
+		return this;
+	}
+
+	public void removeNode(GraphNode targetNode) {
+		this.nodes.remove(targetNode);
+		Iterator<GraphTriple> it = this.triples.iterator();
+		GraphTriple graphTriple = null;
+		Collection<GraphTriple> toRemove = new HashSet<GraphTriple>();
+		while (it.hasNext())
+			graphTriple = it.next();
+		if (graphTriple.fromNode.equals(targetNode.getLabel())
+				|| graphTriple.toNode.equals(targetNode.getLabel())) {
+			toRemove.add(graphTriple);
+		}
+		this.triples.removeAll(toRemove);
+	}
+
+}
