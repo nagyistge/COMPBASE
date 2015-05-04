aebf541d92a6c3bb14a28ad6cab0efc3a6aae270
uzuzjmd <julian.dehne@gmail.com>
2015-05-04 12:27:57 +0200

    fixed startCompetenceServer.sh script
    Former-commit-id: 95e4c325940a8692a60786f000b1691b4f947d73

--- startCompetenceServer.sh
+++ startCompetenceServer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd deployment
-(while true; do sleep 10000; done) | java -jar CompetenceServer.jar tdb http://fleckenroller.cs.uni-potsdam.de > competence.log &
+(while true; do sleep 10000; done) | java -jar CompetenceServer.jar tdb http://fleckenroller.cs.uni-potsdam.de/app > competence.log &
 cd ..
 
