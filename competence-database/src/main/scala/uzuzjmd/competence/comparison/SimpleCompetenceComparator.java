fe81b5fd797efebe38efbd6b0d085ed61d4efe4a
julian.dehne@gmail.com <uzuzjmd12>
2015-08-05 09:36:07 +0200

    added theasaurus spike
    Former-commit-id: 296a973e094942d8932299d25105ecb15fb03dc3

---
+++ competence-database/src/main/scala/uzuzjmd/competence/comparison/SimpleCompetenceComparator.java
@@ -0,0 +1,13 @@
+package uzuzjmd.competence.comparison;
+
+import uzuzjmd.competence.owl.dao.Competence;
+
+public class SimpleCompetenceComparator implements CompetenceComparator {
+
+	@Override
+	public Boolean isSimilar(Competence dao1, Competence dao2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ competence-database/src/main/scala/uzuzjmd/competence/comparison/analysis/SentenceAnalyser.scala
@@ -0,0 +1,66 @@
+package uzuzjmd.competence.comparison.analysis
+
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser
+import edu.stanford.nlp.trees.PennTreebankLanguagePack
+import uzuzjmd.competence.owl.access.MagicStrings
+import scala.collection.JavaConverters._
+import edu.stanford.nlp.ling.Sentence
+import edu.stanford.nlp.trees.Tree
+
+/**
+ * @author dehne
+ * 
+ * SubObjects of this trait are filter to get a specific word class out of a sentence.
+ */
+trait SentenceAnalyser {
+  
+  
+  
+  def hitList = List("VVIZU", "VVINF", "VVFIN", "VVPP", "ADJD") // as an example all verbs
+  
+  /**
+   * 
+   */
+  def convertSentenceToFilteredElement(input: String): List[String] = {    
+    val lp = LexicalizedParser.loadModel(MagicStrings.GERMANMODELLOCATION, "-maxLength", "80");
+    val tlp = new PennTreebankLanguagePack();
+    // Uncomment the following line to obtain original Stanford Dependencies
+    // tlp.setGenerateOriginalDependencies(true);
+    //    val gsf = tlp.grammaticalStructureFactory();
+    val sent = input.split(" ").toList
+    val parse = lp.apply(Sentence.toWordList(sent.asJava));
+
+    //    val gs = gsf.newGrammaticalStructure(parse);
+    //    val tdl = gs.typedDependenciesCCprocessed();
+    //    println(tdl.toString())
+
+    val result = treeWalker(parse)
+
+    return result
+  }
+  
+  def convertSentencetofilteredElementStemmed(input : String) : List[String] = {
+    return convertSentenceToFilteredElement(input).map { x => WordToStem.stemWord(x) }
+  }
+
+  
+  
+  /**
+   * recursively walks the dependency tree in order to find all the words given the specified filter
+   */
+  def treeWalker(input: Tree): List[String] = {
+    val startList = List(): List[String]
+    return treeWalker_Helper(input, startList)
+  }
+
+
+  def treeWalker_Helper(input: Tree, startList: List[String]): List[String] = {
+    if (hitList.forall(x => !input.nodeString().contains(x))) {
+      return startList ::: input.children().map(x => treeWalker_Helper(x, startList)).flatten.toList
+    } else {
+      val newList = input.getChild(0).toString() :: startList
+      return startList ::: input.children().map(x => treeWalker_Helper(x, newList)).flatten.toList
+    }
+
+  }
+}
