52bb3246d106b678ec7f852c80c19577bd7d92bb
julian.dehne@gmail.com <uzuzjmd12>
2015-08-06 13:25:44 +0200

    implemented competence verb comparison

--- competence-database/src/main/scala/uzuzjmd/competence/comparison/analysis/SentenceToOperator.scala
+++ competence-database/src/main/scala/uzuzjmd/competence/comparison/analysis/SentenceToOperator.scala
@@ -10,8 +10,12 @@ import edu.stanford.nlp.trees.Tree
 /**
  * @author dehne
  */
-object SentenceToVerb extends SentenceAnalyser {
+object SentenceToOperator extends SentenceAnalyser {
+
+
 
   override def hitList = List("VVIZU", "VVINF", "VVFIN", "VVPP", "ADJD")
+
+
 
 }
---
+++ competence-database/src/main/scala/uzuzjmd/competence/comparison/simple/SimpleCompetenceComparator.java
@@ -0,0 +1,24 @@
+package uzuzjmd.competence.comparison.simple;
+
+import uzuzjmd.competence.comparison.CompetenceComparator;
+import uzuzjmd.competence.comparison.simple.mapper.SimpleCompetenceComparatorMapper;
+import uzuzjmd.competence.owl.dao.Competence;
+
+public abstract class SimpleCompetenceComparator implements CompetenceComparator {
+
+	@Override
+	public Boolean isSimilar(Competence dao1, Competence dao2) {
+
+		String firstCompetence = dao1.getDefinition();
+		String secondCompetence = dao2.getDefinition();
+
+		// TODO Auto-generated method stub
+		return isSimilarStrings(firstCompetence, secondCompetence);
+	}
+
+	public Boolean isSimilarStrings(String input1, String input2) {
+		SimpleCompetenceComparatorMapper mapper = new SimpleCompetenceComparatorMapper();
+		return mapper.isSimilarStrings(input1, input2);
+
+	}
+}
