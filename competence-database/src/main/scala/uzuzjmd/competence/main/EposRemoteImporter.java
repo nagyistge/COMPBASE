458743d558e436fd90f9ee9526413485e724674b
julian.dehne@gmail.com <uzuzjmd12>
2015-08-13 10:23:36 +0200

    added remote possibility to upload epos competences
    Former-commit-id: 68c36146953e6d963c3055f3f404d68d8232b91f

---
+++ competence-database/src/main/scala/uzuzjmd/competence/main/EposRemoteImporter.java
@@ -0,0 +1,53 @@
+package uzuzjmd.competence.main;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBException;
+
+import org.apache.commons.io.IOUtils;
+
+import uzuzjmd.competence.datasource.epos.DESCRIPTORSETType;
+import uzuzjmd.competence.owl.access.MagicStrings;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class EposRemoteImporter {
+
+	public static void main(String[] args) throws JAXBException, IOException {
+
+		if (args.length < 2) {
+			System.err.println("Usage is java -jar EposRemoteImporter path/to/epos.xml serverurl");
+		}
+
+		else {
+			MagicStrings.EPOSLocation = args[0];
+			MagicStrings.RESTURLCompetence = args[1];
+		}
+
+		List<DESCRIPTORSETType> eposCompetences = EposImporter.parseEPOSXML();
+
+		Client client = Client.create();
+
+		WebResource webResource = client.resource(MagicStrings.RESTURLCompetence + "/competences/xml/learningtemplates/addEpos");
+
+		System.out.println(webResource.getURI());
+
+		ClientResponse response = webResource.accept(MediaType.APPLICATION_XML).post(ClientResponse.class, eposCompetences.toArray(new DESCRIPTORSETType[0]));
+
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+		}
+
+		InputStream output = response.getEntityInputStream();
+		String result = IOUtils.toString(output, "UTF-8");
+
+		System.out.println(result);
+
+		client.destroy();
+	}
+}
