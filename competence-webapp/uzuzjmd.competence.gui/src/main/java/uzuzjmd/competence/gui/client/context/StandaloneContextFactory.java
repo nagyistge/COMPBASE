5a751473898dbe189b60e3145e554327c86ac6be
julian.dehne@gmail.com <uzuzjmd12>
2015-09-09 10:11:23 +0200

    updating api after moodle evidence server changes

--- competence-webapp/uzuzjmd.competence.gui/src/main/java/uzuzjmd/competence/gui/client/context/StandaloneContextFactory.java
+++ competence-webapp/uzuzjmd.competence.gui/src/main/java/uzuzjmd/competence/gui/client/context/StandaloneContextFactory.java
@@ -2,17 +2,11 @@ package uzuzjmd.competence.gui.client.context;
 
 import java.util.HashMap;
 
-import org.fusesource.restygwt.client.JsonCallback;
 import org.fusesource.restygwt.client.JsonEncoderDecoder;
-import org.fusesource.restygwt.client.Method;
-import org.fusesource.restygwt.client.Resource;
 
-import uzuzjmd.competence.gui.client.viewcontroller.ContextSelectionController;
-import uzuzjmd.competence.gui.client.viewcontroller.Controller;
 import uzuzjmd.competence.service.rest.client.dto.UserCourseListResponse;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.json.client.JSONValue;
 
 public class StandaloneContextFactory extends LmsContextFactory {
 
@@ -41,55 +35,6 @@ public class StandaloneContextFactory extends LmsContextFactory {
 		return coursesHashmap;
 	}
 
-	public void updateCourses() {
-		if (super.validUserLoggedIn) {
-			Resource resource = new Resource(
-					Controller.contextFactory.getEvidenceServerURL()
-							+ "/lms/courses/moodle/" + getUser());
-			resource.get().send(new JsonCallback() {
-
-				@Override
-				public void onSuccess(Method method, JSONValue response) {
-					GWT.log("received coursesList for User"
-							+ response.toString());
-					// CourseListCodec codec =
-					// GWT.create(CourseListCodec.class);
-					// GWT.log(codec.toString());
-					// UserCourseListResponse courseListResponse = codec
-					// .decode(response);
-
-					// for (UserCourseListItem userCourseListItem :
-					// courseListResponse) {
-					// coursesHashmap.put(userCourseListItem.getName(),
-					// userCourseListItem.getCourseid());
-					// }
-
-					for (int i = 0; i < response.isArray().size(); i++) {
-						Long courseId = Math.round(response.isArray().get(i)
-								.isObject().get("courseid").isNumber()
-								.doubleValue());
-						String name = response.isArray().get(i).isObject()
-								.get("name").toString().replaceAll("\"", "");
-						coursesHashmap.put(name, courseId);
-					}
-
-					ContextSelectionController
-							.initContextController(coursesHashmap.keySet());
-					Controller.reloadController.reload();
-
-					setCourseContext(ContextSelectionController
-							.getSelectedCourseContext());
-
-				}
-
-				@Override
-				public void onFailure(Method method, Throwable exception) {
-
-				}
-			});
-		}
-	}
-
 	public String getRoleFromContext() {
 		return Role.teacher.toString();
 	}
@@ -100,9 +45,14 @@ public class StandaloneContextFactory extends LmsContextFactory {
 
 	@Override
 	public Long getCourseId() {
+
 		GWT.log("using standalone context factory");
 		GWT.log("course context is: " + getCourseContext());
 		GWT.log("course id is: " + coursesHashmap.get(getCourseContext()));
 		return coursesHashmap.get(getCourseContext());
+	}
+
+	public Boolean getValidLoggedIn() {
+		return super.validUserLoggedIn;
 	}
 }
