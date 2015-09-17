4a374120040e339fca11677b6290f73602653554
ngxanh88 <ngxanh88@gmail.com>
2015-09-17 11:19:13 +0200

    LearningTemplateDao impl with error from createOneCompetence

--- competence-reflexion-2/src/main/java/de/unipotsdam/anh/dao/LearningTemplateDao.java
+++ competence-reflexion-2/src/main/java/de/unipotsdam/anh/dao/LearningTemplateDao.java
@@ -10,17 +10,40 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import uzuzjmd.competence.shared.DESCRIPTORSETType;
 import uzuzjmd.competence.shared.StringList;
 import uzuzjmd.competence.shared.dto.EPOSTypeWrapper;
 import uzuzjmd.competence.shared.dto.Graph;
+import uzuzjmd.competence.shared.dto.LearningTemplateResultSet;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class LearningTemplateDao {
 
+	
+	public static synchronized LearningTemplateResultSet getLearningProjectTemplate(String learningTemplateName) {
+		final Client client = ClientBuilder.newClient();
+		
+		try {
+			final WebTarget webResource = client
+					.target("http://localhost:8084/competences/xml/learningtemplate/get/"
+										+ learningTemplateName);
+			LearningTemplateResultSet result = webResource
+												.request(MediaType.APPLICATION_XML)
+												.get(LearningTemplateResultSet.class);
+			return result;
+		} catch ( Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+		
+		return null;
+	}
+	
 	/**
 	 * send new Template to the server
 	 * @param learningTemplateName
@@ -43,18 +66,21 @@ public class LearningTemplateDao {
 		return -1;
 	}
 	
-	public static synchronized int createTemplate(String learningTemplateName, Graph graph) {
+	public static synchronized int createTemplate(String learningTemplateName, LearningTemplateResultSet learningTemplateResultSet) {
 		final Client client = ClientBuilder.newClient();
-		final WebTarget webResource = client.target("http://localhost:8084/competences/json/learningtemplate/add/");
+		
 		try {
-			Gson gson = new GsonBuilder().create();
-			String json = gson.toJson(graph);
+			final WebTarget webResource = client
+					.target("http://localhost:8084/competences/xml/learningtemplate/add/"
+									+ learningTemplateName)
+					.queryParam("learningTemplateResultSet",
+							learningTemplateResultSet);
 			
-			Response response = webResource
-				.queryParam("learningTemplateName", learningTemplateName)
-				.request(MediaType.APPLICATION_JSON)
-				.post(Entity.json(json));
-			return response.getStatus();
+			Response result = webResource.request(MediaType.APPLICATION_XML)
+					.post(Entity.entity(learningTemplateResultSet,
+							MediaType.APPLICATION_XML));
+			
+			return result.getStatus();
 		} catch ( Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -62,6 +88,56 @@ public class LearningTemplateDao {
 		}
 		
 		return -1;
+	}
+	
+	/**
+	 * add a competence to the model
+	 * @param competence
+	 * @param operator
+	 * @param learningTemplate
+	 * @param catchWords
+	 * @return
+	 */
+	public static synchronized int createOneCompetence(String competence, String operator, String learningTemplate, String... catchWords) {
+		final Client client = ClientBuilder.newClient();
+		final WebTarget webResource = client.target("http://localhost:8084/competences/json/addOne/");
+		try {
+			Response response = webResource
+				.queryParam("competence", competence)
+				.queryParam("operator", operator)
+				.queryParam("catchwords",(Object[]) catchWords)
+				.queryParam("learningTemplateName", learningTemplate)
+				.request()
+				.post(null);
+			return response.getStatus();
+		} catch ( Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+		return -1;
+	}
+	
+	/**
+	 * GET the operator for a given competence
+	 * @param competence
+	 * @return
+	 */
+	public static synchronized String getOperatorFromCompetence(String competence) {
+		final Client client = ClientBuilder.newClient();
+		final WebTarget webResource = client.target("http://localhost:8084/competences/json/operator/");
+		try {
+			Response response = webResource
+				.queryParam("competence", competence)
+				.request()
+				.post(null);
+			return response.readEntity(String.class);
+		} catch ( Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+		return null;
 	}
 	
 	/**
@@ -137,5 +213,21 @@ public class LearningTemplateDao {
 //		} catch(Exception e) {
 //			e.printStackTrace();
 //		}
+	}
+	
+	
+	public static synchronized DESCRIPTORSETType getEposFrom(String learningTemplate) {
+		try {
+			JAXBContext jc = JAXBContext.newInstance(DESCRIPTORSETType.class);
+	
+	        Unmarshaller unmarshaller = jc.createUnmarshaller();
+	        DESCRIPTORSETType descriptorsetType = (DESCRIPTORSETType) unmarshaller.unmarshal(new File("C:/Users/fides-WHK/Wissensmodellierung/competence-reflexion-2/epos-ex.xml"));
+	        
+	        return descriptorsetType;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
