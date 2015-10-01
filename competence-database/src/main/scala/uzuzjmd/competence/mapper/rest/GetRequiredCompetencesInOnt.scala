16a28dfc313c72c4a4984ac7a7e139639dd82354
Jan Bernoth <janbernoth@gmail.com>
2015-10-01 17:13:51 +0200

    Updated Service Rest JSON
    Added new Scala Classes to wrap the wrapper. Wraptastic!
    Added new Classes to handle data

---
+++ competence-database/src/main/scala/uzuzjmd/competence/mapper/rest/GetRequiredCompetencesInOnt.scala
@@ -0,0 +1,24 @@
+package uzuzjmd.competence.mapper.rest
+
+import uzuzjmd.competence.owl.access.TDBWriteTransactional
+import uzuzjmd.competence.owl.access.TDBREADTransactional
+import uzuzjmd.competence.owl.access.CompOntologyManager
+import uzuzjmd.competence.owl.dao.Competence
+
+/**
+* @author jbe
+**/
+
+object GetRequiredCompetencesInOnt extends TDBREADTransactional[String, Array[String]]{
+  def convert(changes:String) : Array[String]= {
+    return execute(convertGetRequiredCompetencesInOnt _, changes)
+  }
+  
+  def convertGetRequiredCompetencesInOnt(comp: CompOntologyManager, changes: String) : Array[String]= {
+    comp.startReasoning();
+		comp.switchOffDebugg();
+		val competence = new Competence(comp, changes, null, null);
+		return competence.getRequiredCompetencesAsArray();
+
+  }
+}
