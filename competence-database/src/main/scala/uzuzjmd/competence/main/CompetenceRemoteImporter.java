a0df0abe5d2fcc2fb3dc1d196af27de810d08b71
julian.dehne@gmail.com <uzuzjmd12>
2015-10-26 11:31:28 +0100

    committing fix for servlet property search

--- competence-database/src/main/scala/uzuzjmd/competence/main/CompetenceRemoteImporter.java
+++ competence-database/src/main/scala/uzuzjmd/competence/main/CompetenceRemoteImporter.java
@@ -10,23 +10,28 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 import uzuzjmd.competence.csv.CompetenceBean;
-import uzuzjmd.competence.owl.access.MagicStrings;
 
 public class CompetenceRemoteImporter {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)
+			throws IOException {
 		// TODO Auto-generated method stub
 
 		// CompetenceImporter importer = new CompetenceImporter();
-		List<CompetenceBean> competenceBeans = CompetenceImporter.parseCompetenceBeans();
+		List<CompetenceBean> competenceBeans = CompetenceImporter
+				.parseCompetenceBeans();
 
 		Client client = ClientBuilder.newClient();
 		// client.setConnectTimeout(300);
 		// client.setReadTimeout(300);
 
-		WebTarget webResource = client.target(MagicStrings.RESTURLCompetence + "/competences/xml/addCompetenceBean");
+		WebTarget webResource = client
+				.target("http://fleckenroller.cs.uni-potsdam.de/app/competence-servlet/competence/competences/xml/addCompetenceBean");
 		System.out.println(webResource.getUri());
-		webResource.request(MediaType.APPLICATION_XML).post(Entity.entity(competenceBeans.toArray(new CompetenceBean[0]), MediaType.APPLICATION_XML));
+		webResource.request(MediaType.APPLICATION_XML)
+				.post(Entity.entity(competenceBeans
+						.toArray(new CompetenceBean[0]),
+						MediaType.APPLICATION_XML));
 
 	}
 }
