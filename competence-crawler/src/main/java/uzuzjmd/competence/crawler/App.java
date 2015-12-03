7a88e2809a6dc9bbc1c74155608a05273c791f86
Jan Bernoth <janbernoth@gmail.com>
2015-12-03 16:31:31 +0100

    First step for StormCrawler
    Init with the default files which are needed for a StormCrawler
    Instance. Further adjustements for own purposes are coming.

---
+++ competence-crawler/src/main/java/uzuzjmd/competence/crawler/App.java
@@ -0,0 +1,13 @@
+package uzuzjmd.competence.crawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
