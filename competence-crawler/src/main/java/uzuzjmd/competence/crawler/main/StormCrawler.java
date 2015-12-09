332974f8eb3f4212a9a11796cf52e19b8cff5708
Jan Bernoth <janbernoth@gmail.com>
2015-12-09 13:42:59 +0100

    Tried to implement the crawler a bit further. Still a hacky thing

--- competence-crawler/src/main/java/uzuzjmd/competence/crawler/main/StormCrawler.java
+++ competence-crawler/src/main/java/uzuzjmd/competence/crawler/main/StormCrawler.java
@@ -3,8 +3,12 @@ package uzuzjmd.competence.crawler.main;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
+import org.elasticsearch.storm.EsBolt;
+
+import backtype.storm.topology.TopologyBuilder;
 
 import com.digitalpebble.storm.crawler.ConfigurableTopology;
+import com.digitalpebble.storm.*;
 
 public class StormCrawler extends ConfigurableTopology {
 
@@ -17,8 +21,40 @@ public class StormCrawler extends ConfigurableTopology {
 		}
 		logger.debug("Entering run with " + myArgs);
 		// TODO Auto-generated method stub
-		logger.debug("Leaving run");
-		return 0;
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("spout", new RandomSentenceSpout(), 10);
+        builder.setBolt("esBolt", new EsBolt("twitter/tweets"));
+        //builder.setSpout("spout", new ElasticSearchSpout());
+
+        /*
+        builder.setBolt("partitioner", new URLPartitionerBolt())
+                .shuffleGrouping("esBolt");
+
+        builder.setBolt("fetch", new FetcherBolt()).fieldsGrouping(
+                "partitioner", new Fields("key"));
+
+        builder.setBolt("sitemap", new SiteMapParserBolt())
+                .localOrShuffleGrouping("fetch");
+
+        builder.setBolt("parse", new JSoupParserBolt()).localOrShuffleGrouping(
+                "sitemap");
+
+        builder.setBolt("indexer", new IndexerBolt()).localOrShuffleGrouping(
+                "parse");
+
+        builder.setBolt("status", new StatusUpdaterBolt())
+                .localOrShuffleGrouping("fetch", Constants.StatusStreamName)
+                .localOrShuffleGrouping("sitemap", Constants.StatusStreamName)
+                .localOrShuffleGrouping("parse", Constants.StatusStreamName)
+                .localOrShuffleGrouping("indexer", Constants.StatusStreamName);
+
+        conf.registerMetricsConsumer(MetricsConsumer.class);
+
+        int result = submit("crawl", conf, builder);
+        */
+        int result = 1;
+        logger.debug("Leaving run with " + result);
+        return result;
 	}
 	
 	public static void main(String[] args) {
