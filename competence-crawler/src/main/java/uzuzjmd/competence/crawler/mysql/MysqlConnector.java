5efe2e4c406e63210c53dd6e377dbe1991a7541c
Jan Bernoth <janbernoth@gmail.com>
2016-02-07 13:08:32 +0100

    Now it runs with big data

--- competence-crawler/src/main/java/uzuzjmd/competence/crawler/mysql/MysqlConnector.java
+++ competence-crawler/src/main/java/uzuzjmd/competence/crawler/mysql/MysqlConnector.java
@@ -2,13 +2,12 @@ package uzuzjmd.competence.crawler.mysql;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.specs2.internal.scalaz.Alpha;
-import uzuzjmd.competence.comparison.synonyms.OpenThesaurusSynonymCreator;
 import uzuzjmd.competence.config.MagicStrings;
 import uzuzjmd.competence.crawler.exception.NoResultsException;
 import uzuzjmd.database.mysql.MysqlConnect;
 import uzuzjmd.database.mysql.VereinfachtesResultSet;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,9 +18,39 @@ public class MysqlConnector {
       "/Hochschulen" +
       "?user=" + MagicStrings.thesaurusLogin +
       "&password=" + MagicStrings.thesaurusPassword;
+    private VereinfachtesResultSet hochschulen;
+    private List<MysqlResult> mysqlResults;
 
     static private final Logger logger = LogManager.getLogger(MysqlConnector.class.getName());
 
+    public MysqlConnector() {
+        MysqlConnect connector = new MysqlConnect();
+        connector.connect(connectionString);
+        hochschulen = connector.issueSelectStatement("Select * from Hochschulen");
+        mysqlResults = new ArrayList<>();
+        while (hochschulen.next() ) {
+            MysqlResult msr = new MysqlResult();
+            msr.bundesland = hochschulen.getString("Bundesland");
+            msr.hochschulname = hochschulen.getString("Hochschulname");
+            msr.hochschultyp = hochschulen.getString("Hochschultyp");
+            msr.homepage = hochschulen.getString("Homepage");
+            msr.lat = hochschulen.getDouble("lat");
+            msr.lon = hochschulen.getDouble("lon");
+            mysqlResults.add(msr);
+        }
+    }
+
+    public MysqlResult searchDomain(String domain) throws NoResultsException {
+        logger.debug("Entering searchDomain with domain:" + domain);
+        for (MysqlResult msr :
+                mysqlResults) {
+            if (msr.homepage.contains(domain)) {
+                return msr;
+            }
+        }
+        logger.debug("Leaving queryDomain with 0 fetches");
+        throw new NoResultsException("No Domain found");
+    }
     public VereinfachtesResultSet queryDomain(String domain) throws NoResultsException {
         logger.debug("Entering queryDomain with domain:" + domain);
         MysqlConnect connector = new MysqlConnect();
