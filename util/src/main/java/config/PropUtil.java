ec60c7b7912b42d7e58dfd02f9396ea48e096444
Jan Bernoth <janbernoth@gmail.com>
2016-03-04 12:27:05 +0100

    Added some mysql features

--- util/src/main/java/config/PropUtil.java
+++ util/src/main/java/config/PropUtil.java
@@ -1,9 +1,6 @@
 package config;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Properties;
 
