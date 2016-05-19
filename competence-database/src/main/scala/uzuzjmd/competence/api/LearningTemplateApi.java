035bcc0ba96ff6c470f9ca2c5a8cbe44c76c440e
julian.dehne@gmail.com <uzuzjmd12>
2016-05-19 15:24:28 +0200

    solves warnings of ambiguitiy in api1 between interface and implications

--- competence-database/src/main/scala/uzuzjmd/competence/api/LearningTemplateApi.java
+++ competence-database/src/main/scala/uzuzjmd/competence/api/LearningTemplateApi.java
@@ -3,26 +3,20 @@ package uzuzjmd.competence.api;
 import uzuzjmd.competence.service.rest.dto.LearningTemplateData;
 import uzuzjmd.competence.shared.StringList;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 /**
  * Created by dehne on 15.04.2016.
  */
 public interface LearningTemplateApi {
-    @Path("/learningtemplate")
-    @GET
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+
     StringList getLearningTemplates();
 
-    @Path("/learningtemplate/{learningtemplateId}")
-    @PUT
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+
     Response addLearningTemplate(@PathParam("learningtemplateId") String learningtemplateId, LearningTemplateData data);
 
-    @Path("/learningtemplate/{learningtemplateId}")
-    @DELETE
+
     Response deleteLearningTemplate(@PathParam("learningtemplateId") String learningtemplateId);
 
     /**
@@ -30,8 +24,6 @@ public interface LearningTemplateApi {
      * @param learningtemplateId
      * @return
      */
-    @Path("/learningtemplate/{learningtemplateId}/delete")
-    @POST
     Response deleteLearningTemplateLegacy(@PathParam("learningtemplateId") String learningtemplateId);
 
     /**
@@ -41,8 +33,5 @@ public interface LearningTemplateApi {
      * @param data
      * @return
      */
-    @Path("/learningtemplate/{learningtemplateId}/create")
-    @POST
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     Response addLearningTemplateLegacy(@PathParam("learningtemplateId") String learningtemplateId, LearningTemplateData data);
 }
