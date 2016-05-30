302d3c6fd120b09a4828bbe0700cdf6985a4011f
julian.dehne@gmail.com <uzuzjmd12>
2016-05-30 14:01:39 +0200

    fixes #68

--- competence-tests/src/main/java/uzuzjmd/competence/tests/SubCompetencenTest.java
+++ competence-tests/src/main/java/uzuzjmd/competence/tests/SubCompetencenTest.java
@@ -9,20 +9,19 @@ import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import scala.collection.immutable.List;
 import uzuzjmd.competence.evidence.service.rest.EvidenceServiceRestServerImpl;
 import uzuzjmd.competence.persistence.dao.DBInitializer;
-import uzuzjmd.competence.service.rest.CompetenceApiImpl;
-import uzuzjmd.competence.service.rest.CourseApiImpl;
-import uzuzjmd.competence.service.rest.EvidenceApiImpl;
-import uzuzjmd.competence.service.rest.LearningTemplateApiImpl;
-import uzuzjmd.competence.service.rest.UserApiImpl;
+import uzuzjmd.competence.service.rest.*;
+
+import java.util.Arrays;
 
 public class SubCompetencenTest extends JerseyTest {
 
 	@Override
     protected javax.ws.rs.core.Application configure() {
         DBInitializer.init();
-        return new ResourceConfig(LearningTemplateApiImpl.class, CompetenceApiImpl.class, UserApiImpl.class, CourseApiImpl.class, EvidenceApiImpl.class, EvidenceServiceRestServerImpl.class);
+		return new ResourceConfig(LearningTemplateApiImpl.class, CompetenceApiImpl.class, UserApiImpl.class, CourseApiImpl.class, EvidenceApiImpl.class, EvidenceServiceRestServerImpl.class, CompetenceServiceRestJSON.class);
     }
 	
 	@Test
@@ -30,31 +29,35 @@ public class SubCompetencenTest extends JerseyTest {
 		final int status = createSubCompetence("creating api", "unter Kompetence", "op1", "catW 1");
 		final int status1 = createSubCompetence("creating api", "unter Kompetence 1", "op1", "catW 1");
 		final int status2 = createSubCompetence("creating api", "unter Kompetence 2", "op1", "catW 1");
-		
-		String subCompetencen = getSubCompetence("creating api");
-		
-		System.out.println(subCompetencen);
-		
 		Assert.assertEquals(200, status);
 		Assert.assertEquals(200, status1);
 		Assert.assertEquals(200, status2);
+
+
+		String subCompetencen = getSubCompetence("creating api");
+		
+		System.out.println(subCompetencen);
+
 	}
 	
 	public int createSubCompetence(String superCompetence, String subCompetence, String operator, String ... catchwords) {
 		final Response response = target("/competences/addOne/")
 								.queryParam("competence", superCompetence)
 								.queryParam("operator", operator)
-								.queryParam("catchwords", (Object [])catchwords)
-								.queryParam("subCompetences", subCompetence)
-								.request().put(Entity.entity("", MediaType.APPLICATION_JSON));
+								.queryParam("superCompetences", List.empty())
+								.queryParam("catchwords", Arrays.asList(catchwords))
+								.queryParam("subCompetences", Arrays.asList(subCompetence))
+								.request().post(null);
 		
 		return response.getStatus();
 	}
 	
 	public String getSubCompetence(String rootCompetence) {
 		final String context = "university";
-		final String response = target("/competences/competencetree/" + context + "/")
+		final String response = target("/api1/competences")
 				.queryParam("rootCompetence", rootCompetence)
+				.queryParam("asTree", false)
+				.queryParam("courseId", context)
 				.request().get(String.class);
 		return response;
 	}
